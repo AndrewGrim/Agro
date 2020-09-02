514b9b83555e7387ea890544c0048e1b3b9f02af
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-09-02 09:49:13 +0100

    Added function that will be used for pretty printing

--- src/util.hpp
+++ src/util.hpp
@@ -2,6 +2,14 @@
     #define UTIL_HPP
         #include <iostream>
 
+        template <typename T> void pprint(int indent, T message, bool newline = true) {
+            for (int i = 0; i < indent; i++) {
+                std::cout << " ";
+            }
+            std::cout << message;
+            if (newline) std::cout << "\n";
+        }
+
         template <typename T> void print(T message) {
             std::cout << message;
         }
