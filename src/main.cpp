b779d92baaefe58191eec062335ebcc83633b483
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-09-10 20:41:36 +0100

    Added size to window, when window is resized update size and redraw

--- src/main.cpp
+++ src/main.cpp
@@ -14,14 +14,17 @@ class Application {
         SDL_Renderer *ren;
         Color bg = {155, 155, 155, 255};
         Widget *main_widget;
+        Size m_size;
 
-        Application(const char* title = "Application", int width = 400, int height = 400) {
+        Application(const char* title = "Application", Size size = Size { 400, 400 }) {
+            this->m_size = size;
+
             SDL_Init(SDL_INIT_VIDEO);
             this->win = SDL_CreateWindow(
                 title, 
                 SDL_WINDOWPOS_CENTERED, 
                 SDL_WINDOWPOS_CENTERED, 
-                width, height,
+                size.width, size.height,
                 SDL_WINDOW_RESIZABLE
             ); 
             this->ren = SDL_CreateRenderer(this->win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
@@ -37,7 +40,7 @@ class Application {
             SDL_SetRenderDrawColor(this->ren, this->bg.red, this->bg.green, this->bg.blue, this->bg.alpha);
             SDL_RenderClear(this->ren);
 
-            this->main_widget->draw(this->ren, Rect { 0, 0, 400, 400 }); // TODO this need the window size
+            this->main_widget->draw(this->ren, Rect { 0, 0, this->m_size.width, this->m_size.height });
         }
 
         void set_main_widget(Widget *widget) {
@@ -58,7 +61,14 @@ class Application {
                             int x, y;
                             SDL_GetMouseState(&x, &y);
                             std::cout << "Mouse(" << x << ", " << y << ")\n";
-                            this->show(); // TODO probably some sort of update function instead of using show manually
+                            break;
+                        case SDL_WINDOWEVENT:
+                            switch (event.window.event) {
+                                case SDL_WINDOWEVENT_RESIZED:
+                                    this->m_size = Size { event.window.data1, event.window.data2 };
+                                    this->show(); // TODO probably some sort of update function instead of using show manually
+                                    break;
+                            }
                             break;
                         case SDL_QUIT:
                             goto EXIT;
