1071a4a7e93bb3688036d951174dbe655cace7eb
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-09-26 21:42:59 +0100

    dc: Added drawBorder function

--- src/controls/button.hpp
+++ src/controls/button.hpp
@@ -24,7 +24,7 @@
                 // TODO get rid of app, and init with dc, this could be done by messing with the widget in append.
                 this->dc = dc;
                 this->rect = rect;
-                this->set_size(this->dc->measureText(text()));
+                this->set_size(dc->measureText(text()));
                 Color color = Color(0, 0, 0, 0); 
                 if (this->is_pressed() && this->is_hovered()) {
                     color = this->pressed_background(); 
@@ -34,84 +34,9 @@
                     color = this->background();
                 }
                 
-                // light border
-                {
-                    // bottom layer of the top & left border : white, drawn first so that the top layer will paint over some extra pixels from here
-                    dc->fillRect(
-                        Rect<float>(rect.x,
-                            rect.y + this->m_border_width / 2,
-                            rect.w,
-                            this->m_border_width / 2
-                        ), Color(1.0f, 1.0f, 1.0f)
-                    );
-                    dc->fillRect(
-                        Rect<float>(rect.x + this->m_border_width / 2,
-                            rect.y,
-                            this->m_border_width / 2,
-                            rect.h
-                        ), Color(1.0f, 1.0f, 1.0f)
-                    );
-                    // top layer of the top & left border : background
-                    dc->fillRect(
-                        Rect<float>(rect.x,
-                            rect.y,
-                            rect.w,
-                            this->m_border_width / 2
-                        ), this->background()
-                    );
-                    dc->fillRect(
-                        Rect<float>(rect.x,
-                            rect.y,
-                            this->m_border_width / 2,
-                            rect.h
-                        ), this->background()
-                    );
-                }
-                // dark border
-                {
-                    // top layer of the bottom & right border : dark grey, drawn first so that the bottom layer will paint over some extra pixels from here
-                    dc->fillRect(
-                        Rect<float>(rect.x + this->m_border_width / 2,
-                            rect.y + rect.h - this->m_border_width,
-                            rect.w - this->m_border_width / 2,
-                            this->m_border_width / 2
-                        ), Color(0.4f, 0.4f, 0.4f)
-                    );
-                    dc->fillRect(
-                        Rect<float>(rect.x + rect.w - this->m_border_width,
-                            rect.y + this->m_border_width / 2,
-                            this->m_border_width / 2,
-                            rect.h - this->m_border_width / 2
-                        ), Color(0.4f, 0.4f, 0.4f)
-                    );
-                    // bottom layer of the bottom & right border : black
-                    dc->fillRect(
-                        Rect<float>(rect.x,
-                            rect.y + rect.h - this->m_border_width / 2,
-                            rect.w,
-                            this->m_border_width / 2
-                        ), Color(0.0f, 0.0f, 0.0f)
-                    );
-                    dc->fillRect(
-                        Rect<float>(rect.x + rect.w - this->m_border_width / 2,
-                            rect.y,
-                            this->m_border_width / 2,
-                            rect.h
-                        ), Color(0.0f, 0.0f, 0.0f)
-                    );
-                }
-
-                // resize rectangle to account for border
-                this->rect = Rect<float>(
-                    rect.x + this->m_border_width, 
-                    rect.y + this->m_border_width, 
-                    rect.w - this->m_border_width * 2, 
-                    rect.h - this->m_border_width * 2
-                );
-                // actual rectangle
-                dc->fillRect(this->rect, color);
-
-                // draw text
+                // draw border and shrink rectangle to prevent drawing over the border
+                rect = dc->drawBorder(rect, this->m_border_width, color);
+                dc->fillRect(rect, color);
                 dc->drawTextAligned(this->m_text, this->m_text_align, rect, this->m_padding + this->m_border_width / 2);
             }
 
--- src/renderer/drawing_context.hpp
+++ src/renderer/drawing_context.hpp
@@ -101,6 +101,85 @@
                 }
             }
 
+            Rect<float> drawBorder(Rect<float> rect, int border_width, Color rect_color) {
+                // light border
+                {
+                    // bottom layer of the top & left border : white, drawn first so that the top layer will paint over some extra pixels from here
+                    this->fillRect(
+                        Rect<float>(rect.x,
+                            rect.y + border_width / 2,
+                            rect.w,
+                            border_width / 2
+                        ), Color(1.0f, 1.0f, 1.0f)
+                    );
+                    this->fillRect(
+                        Rect<float>(rect.x + border_width / 2,
+                            rect.y,
+                            border_width / 2,
+                            rect.h
+                        ), Color(1.0f, 1.0f, 1.0f)
+                    );
+                    // top layer of the top & left border : background
+                    this->fillRect(
+                        Rect<float>(rect.x,
+                            rect.y,
+                            rect.w,
+                            border_width / 2
+                        ), rect_color
+                    );
+                    this->fillRect(
+                        Rect<float>(rect.x,
+                            rect.y,
+                            border_width / 2,
+                            rect.h
+                        ), rect_color
+                    );
+                }
+                // dark border
+                {
+                    // top layer of the bottom & right border : dark grey, drawn first so that the bottom layer will paint over some extra pixels from here
+                    this->fillRect(
+                        Rect<float>(rect.x + border_width / 2,
+                            rect.y + rect.h - border_width,
+                            rect.w - border_width / 2,
+                            border_width / 2
+                        ), Color(0.4f, 0.4f, 0.4f)
+                    );
+                    this->fillRect(
+                        Rect<float>(rect.x + rect.w - border_width,
+                            rect.y + border_width / 2,
+                            border_width / 2,
+                            rect.h - border_width / 2
+                        ), Color(0.4f, 0.4f, 0.4f)
+                    );
+                    // bottom layer of the bottom & right border : black
+                    this->fillRect(
+                        Rect<float>(rect.x,
+                            rect.y + rect.h - border_width / 2,
+                            rect.w,
+                            border_width / 2
+                        ), Color(0.0f, 0.0f, 0.0f)
+                    );
+                    this->fillRect(
+                        Rect<float>(rect.x + rect.w - border_width / 2,
+                            rect.y,
+                            border_width / 2,
+                            rect.h
+                        ), Color(0.0f, 0.0f, 0.0f)
+                    );
+                }
+
+                // resize rectangle to account for border
+                rect = Rect<float>(
+                    rect.x + border_width, 
+                    rect.y + border_width, 
+                    rect.w - border_width * 2, 
+                    rect.h - border_width * 2
+                );
+
+                return rect;
+            }
+
             void clear() {
                 glClearColor(0, 0, 0, 1);
                 glClear(GL_COLOR_BUFFER_BIT);
