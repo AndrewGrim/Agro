fb54c8f1d67f2efc78fe9de58d1354f89a998ee5
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-09-27 10:21:40 +0100

    Fixed narrowing conversion warning

--- src/renderer/text_renderer.h
+++ src/renderer/text_renderer.h
@@ -24,7 +24,7 @@ struct Character {
     unsigned int TextureID; // ID handle of the glyph texture
     glm::ivec2   Size;      // size of glyph
     glm::ivec2   Bearing;   // offset from baseline to left/top of glyph
-    unsigned int Advance;   // horizontal offset to advance to next glyph
+    long int Advance;   // horizontal offset to advance to next glyph
 };
 
 
