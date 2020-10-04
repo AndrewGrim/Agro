97baf2c5e71d8aa591045e461843679d34274a78
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-10-04 18:26:31 +0100

    Fixed value calculation for slider

--- src/controls/slider.hpp
+++ src/controls/slider.hpp
@@ -114,23 +114,23 @@
                 return this;
             }
 
-        private:
+        protected:
             const char *m_name = "Slider";
             SliderButton *m_slider_button = nullptr;
     };
 
     void onMouseMove(Widget *slider_button, MouseEvent event) {
         if (slider_button && slider_button->is_pressed()) {
-            if (slider_button->m_parent && slider_button->m_parent->name() == "Slider") {
+            if (slider_button->m_parent) {
                 Slider *parent = (Slider*)slider_button->m_parent;
                 Rect<float> rect = parent->rect;
                 if (parent->m_align_policy == Align::Horizontal) {
-                    float value = parent->m_max - ((rect.x + rect.w - event.x) / (rect.x + rect.w));
+                    float value = parent->m_max - ((rect.x + rect.w - event.x) / (rect.w));
                     if (value > parent->m_max) value = parent->m_max;
                     else if (value < parent->m_min) value = parent->m_min;
                     parent->m_value = value;
                 } else {
-                    float value = parent->m_max - ((rect.y + rect.h - event.y) / (rect.y + rect.h));
+                    float value = parent->m_max - ((rect.y + rect.h - event.y) / (rect.h));
                     if (value > parent->m_max) value = parent->m_max;
                     else if (value < parent->m_min) value = parent->m_min;
                     parent->m_value = value;
