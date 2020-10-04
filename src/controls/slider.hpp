868dc1bebe12e419b293412ffcb488a1cf1db65c
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-10-04 15:25:33 +0100

    Fixed bug where the slider would return a value outside of its range

--- src/controls/slider.hpp
+++ src/controls/slider.hpp
@@ -96,9 +96,15 @@
                 Slider *parent = (Slider*)button->m_parent;
                 Rect<float> rect = parent->rect;
                 if (parent->m_align_policy == Align::Horizontal) {
-                    parent->m_value = 1.0 - ((rect.x + rect.w - event.x) / (rect.x + rect.w));
+                    float value = parent->m_max - ((rect.x + rect.w - event.x) / (rect.x + rect.w));
+                    if (value > parent->m_max) value = parent->m_max;
+                    else if (value < parent->m_min) value = parent->m_min;
+                    parent->m_value = value;
                 } else {
-                    parent->m_value = 1.0 - ((rect.y + rect.h - event.y) / (rect.y + rect.h));
+                    float value = parent->m_max - ((rect.y + rect.h - event.y) / (rect.y + rect.h));
+                    if (value > parent->m_max) value = parent->m_max;
+                    else if (value < parent->m_min) value = parent->m_min;
+                    parent->m_value = value;
                 }
                 if (parent->value_changed_callback) parent->value_changed_callback(parent);
                 if (button->m_app) ((Application*)button->m_app)->m_needs_update = true;
