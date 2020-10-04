dac9a2efcde0e30d2a141df2169a421d278a7d5f
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-10-04 19:57:35 +0100

    Connected scrollbar buttons to change the slider value

--- src/controls/scrollbar.hpp
+++ src/controls/scrollbar.hpp
@@ -8,6 +8,9 @@
     #include "slider.hpp"
     #include "../renderer/drawing_context.hpp"
 
+    void _onBeginClicked(Widget *scrollbar_begin_button, MouseEvent event);
+    void _onEndClicked(Widget *scrollbar_begin_button, MouseEvent event);
+
     class ScrollBar : public Widget {
         public:
             Button *m_begin_button = nullptr;
@@ -21,12 +24,16 @@
                 this->m_text = text;
 
                 m_begin_button = new Button("<");
+                m_begin_button->m_parent = this;
+                m_begin_button->mouse_click_callback = _onBeginClicked;
                 this->append(m_begin_button, Fill::None);
 
                 m_slider = new Slider(alignment, text);
                 this->append(m_slider, Fill::Vertical);
 
                 m_end_button = new Button(">");
+                m_end_button->m_parent = this;
+                m_end_button->mouse_click_callback = _onEndClicked;
                 this->append(m_end_button, Fill::None);
             }
             
@@ -68,5 +75,29 @@
 
         protected:
             const char *m_name = "ScrollBar";
-    };
+    };  
+
+    void _onBeginClicked(Widget *scrollbar_begin_button, MouseEvent event) {
+        if (scrollbar_begin_button) {
+            if (scrollbar_begin_button->m_parent) {
+                ScrollBar *parent = (ScrollBar*)scrollbar_begin_button->m_parent;
+                parent->m_slider->m_value -= 0.05f; 
+                if (parent->m_slider->m_value < 0.0f) parent->m_slider->m_value = 0.0f;
+                if (scrollbar_begin_button->m_app) ((Application*)scrollbar_begin_button->m_app)->m_needs_update = true;
+                println(parent->m_slider->m_value);
+            }
+        }
+    }
+
+    void _onEndClicked(Widget *scrollbar_end_button, MouseEvent event) {
+        if (scrollbar_end_button) {
+            if (scrollbar_end_button->m_parent) {
+                ScrollBar *parent = (ScrollBar*)scrollbar_end_button->m_parent;
+                parent->m_slider->m_value += 0.05f; 
+                if (parent->m_slider->m_value > 1.0f) parent->m_slider->m_value = 1.0f;
+                if (scrollbar_end_button->m_app) ((Application*)scrollbar_end_button->m_app)->m_needs_update = true;
+                println(parent->m_slider->m_value);
+            }
+        }
+    }
 #endif
