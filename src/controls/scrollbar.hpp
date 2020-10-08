0d1da5da44fddac84ddcab4d640b7fe54bb51688
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-10-08 15:21:32 +0100

    Split up functions because of redefinition errors

--- src/controls/scrollbar.hpp
+++ src/controls/scrollbar.hpp
@@ -76,28 +76,4 @@
         protected:
             const char *m_name = "ScrollBar";
     };  
-
-    void _onBeginClicked(Widget *scrollbar_begin_button, MouseEvent event) {
-        if (scrollbar_begin_button) {
-            if (scrollbar_begin_button->m_parent) {
-                ScrollBar *parent = (ScrollBar*)scrollbar_begin_button->m_parent;
-                parent->m_slider->m_value -= 0.05f; 
-                if (parent->m_slider->m_value < 0.0f) parent->m_slider->m_value = 0.0f;
-                if (scrollbar_begin_button->m_app) ((Application*)scrollbar_begin_button->m_app)->m_needs_update = true;
-                println(parent->m_slider->m_value);
-            }
-        }
-    }
-
-    void _onEndClicked(Widget *scrollbar_end_button, MouseEvent event) {
-        if (scrollbar_end_button) {
-            if (scrollbar_end_button->m_parent) {
-                ScrollBar *parent = (ScrollBar*)scrollbar_end_button->m_parent;
-                parent->m_slider->m_value += 0.05f; 
-                if (parent->m_slider->m_value > 1.0f) parent->m_slider->m_value = 1.0f;
-                if (scrollbar_end_button->m_app) ((Application*)scrollbar_end_button->m_app)->m_needs_update = true;
-                println(parent->m_slider->m_value);
-            }
-        }
-    }
 #endif
--- src/controls/slider.hpp
+++ src/controls/slider.hpp
@@ -118,26 +118,4 @@
         protected:
             const char *m_name = "Slider";
     };
-
-    void _onMouseMove(Widget *slider_button, MouseEvent event) {
-        if (slider_button && slider_button->is_pressed()) {
-            if (slider_button->m_parent) {
-                Slider *parent = (Slider*)slider_button->m_parent;
-                Rect<float> rect = parent->rect;
-                if (parent->m_align_policy == Align::Horizontal) {
-                    float value = parent->m_max - ((rect.x + rect.w - event.x) / (rect.w));
-                    if (value > parent->m_max) value = parent->m_max;
-                    else if (value < parent->m_min) value = parent->m_min;
-                    parent->m_value = value;
-                } else {
-                    float value = parent->m_max - ((rect.y + rect.h - event.y) / (rect.h));
-                    if (value > parent->m_max) value = parent->m_max;
-                    else if (value < parent->m_min) value = parent->m_min;
-                    parent->m_value = value;
-                }
-                if (parent->value_changed_callback) parent->value_changed_callback(parent);
-                if (slider_button->m_app) ((Application*)slider_button->m_app)->m_needs_update = true;
-            }
-        }
-    }
 #endif
