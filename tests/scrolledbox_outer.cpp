0b83981c4205a0155dc1a8d67dd887aede9ddced
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-11-25 20:10:51 +0000

    Updated test for new default font

--- tests/scrolledbox_outer.cpp
+++ tests/scrolledbox_outer.cpp
@@ -25,25 +25,25 @@ void onApplicationReady(Application *self) {
     assert(((ScrolledBox*)self->main_widget)->has_scrollbar(Align::Vertical));
     assert(((ScrolledBox*)self->main_widget)->has_scrollbar(Align::Horizontal));
 
-    assert(self->main_widget->children[0]->children[0]->rect == Rect<float>(0, 0, 250, 250));
-    assert(self->main_widget->children[0]->children[1]->rect == Rect<float>(250, 0, 250, 250));
-    assert(self->main_widget->children[1]->children[0]->rect == Rect<float>(0, 250, 250, 250));
-    assert(self->main_widget->children[1]->children[1]->rect == Rect<float>(250, 250, 250, 250));
+    assert(self->main_widget->children[0]->children[0]->rect == Rect<float>(0, 0, 300, 300));
+    assert(self->main_widget->children[0]->children[1]->rect == Rect<float>(300, 0, 300, 300));
+    assert(self->main_widget->children[1]->children[0]->rect == Rect<float>(0, 300, 300, 300));
+    assert(self->main_widget->children[1]->children[1]->rect == Rect<float>(300, 300, 300, 300));
 }
 
 int main() { 
-    Application *app = new Application("ScrolledBox Outer Test", Size<int>(400, 400));
+    Application *app = new Application("ScrolledBox Outer Test", Size<int>(500, 500));
         app->ready_callback = onApplicationReady;
 
         Box *top = (new Box(Align::Horizontal))->set_background(Color());
-            ScrolledBox *av_fn = (new ScrolledBox(Align::Vertical, Size<float>(250, 250)))->set_background(Color(0.0));
+            ScrolledBox *av_fn = (new ScrolledBox(Align::Vertical, Size<float>(300, 300)))->set_background(Color(0.0));
                 av_fn->append((new Button("P: Fill::None, C: Fill::None"))->set_background(Color(0.7, 0.2, 0.2)), Fill::None);
                 av_fn->append((new Button("P: Fill::None, C: Fill::Horizontal"))->set_background(Color(0.7, 0.2, 0.2)), Fill::Horizontal);
                 av_fn->append((new Button("P: Fill::None, C: Fill::Vertical"))->set_background(Color(0.7, 0.2, 0.2)), Fill::Vertical);
                 av_fn->append((new Button("P: Fill::None, C: Fill::Both"))->set_background(Color(0.7, 0.2, 0.2)), Fill::Both);
             top->append(av_fn, Fill::None);
 
-            ScrolledBox *av_fb = (new ScrolledBox(Align::Vertical, Size<float>(250, 250)))->set_background(Color(0.0));
+            ScrolledBox *av_fb = (new ScrolledBox(Align::Vertical, Size<float>(300, 300)))->set_background(Color(0.0));
                 av_fb->append((new Button("P: Fill::Both, C: Fill::None"))->set_background(Color(0.7, 0.7, 0.2)), Fill::None);
                 av_fb->append((new Button("P: Fill::Both, C: Fill::Horizontal"))->set_background(Color(0.7, 0.7, 0.2)), Fill::Horizontal);
                 av_fb->append((new Button("P: Fill::Both, C: Fill::Vertical"))->set_background(Color(0.7, 0.7, 0.2)), Fill::Vertical);
@@ -52,14 +52,14 @@ int main() {
         app->append(top, Fill::Both);
 
         Box *bottom = (new Box(Align::Horizontal))->set_background(Color());
-            ScrolledBox *av_fh = (new ScrolledBox(Align::Vertical, Size<float>(250, 250)))->set_background(Color(0.0));
+            ScrolledBox *av_fh = (new ScrolledBox(Align::Vertical, Size<float>(300, 300)))->set_background(Color(0.0));
                 av_fh->append((new Button("P: Fill::Horizontal, C: Fill::None"))->set_background(Color(0.2, 0.7, 0.2)), Fill::None);
                 av_fh->append((new Button("P: Fill::Horizontal, C: Fill::Horizontal"))->set_background(Color(0.2, 0.7, 0.2)), Fill::Horizontal);
                 av_fh->append((new Button("P: Fill::Horizontal, C: Fill::Vertical"))->set_background(Color(0.2, 0.7, 0.2)), Fill::Vertical);
                 av_fh->append((new Button("P: Fill::Horizontal, C: Fill::Both"))->set_background(Color(0.2, 0.7, 0.2)), Fill::Both);
             bottom->append(av_fh, Fill::Horizontal);
 
-            ScrolledBox *av_fv = (new ScrolledBox(Align::Vertical, Size<float>(250, 250)))->set_background(Color(0.0));
+            ScrolledBox *av_fv = (new ScrolledBox(Align::Vertical, Size<float>(300, 300)))->set_background(Color(0.0));
                 av_fv->append((new Button("P: Fill::Vertical, C: Fill::None"))->set_background(Color(0.2, 0.2, 0.7)), Fill::None);
                 av_fv->append((new Button("P: Fill::Vertical, C: Fill::Horizontal"))->set_background(Color(0.2, 0.2, 0.7)), Fill::Horizontal);
                 av_fv->append((new Button("P: Fill::Vertical, C: Fill::Vertical"))->set_background(Color(0.2, 0.2, 0.7)), Fill::Vertical);
