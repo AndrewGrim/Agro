96fd770ddfc2c4395fc8569b17b9e7f74a0cc928
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-11-28 09:55:54 +0000

    Reworked rect to only use floats, added shrink method

--- src/common/rect.hpp
+++ src/common/rect.hpp
@@ -4,35 +4,41 @@
     #include <iostream>
     #include <stdio.h>
 
-    template <class T> class Rect {
-        public:
-            T x;
-            T y;
-            T w;
-            T h;
+    struct Rect {
+        float x;
+        float y;
+        float w;
+        float h;
 
-            Rect(T x = 0.0f, T y = 0.0f, T w = 0.0f, T h = 0.0f) {
-                this->x = x;
-                this->y = y;
-                this->w = w;
-                this->h = h;
+        Rect(float x = 0.0f, float y = 0.0f, float w = 0.0f, float h = 0.0f) {
+            this->x = x;
+            this->y = y;
+            this->w = w;
+            this->h = h;
+        }
+
+        void shrink(float amount) {   
+            this->x + amount;
+            this->y + amount;
+            this->w - amount * 2;
+            this->h - amount * 2;
+        }
+
+        friend bool operator==(const Rect &lhs, const Rect &rhs) {
+            if (lhs.x == rhs.x &&
+                lhs.y == rhs.y &&
+                lhs.w == rhs.w &&
+                lhs.h == rhs.h
+            ) {
+                return true;
             }
 
-            friend bool operator==(const Rect<T> &lhs, const Rect<T> &rhs) {
-                if (lhs.x == rhs.x &&
-                    lhs.y == rhs.y &&
-                    lhs.w == rhs.w &&
-                    lhs.h == rhs.h
-                ) {
-                    return true;
-                }
+            return false;
+        }
 
-                return false;
-            }
-
-            friend std::ostream& operator<<(std::ostream &os, const Rect<T> &rect) {
-                printf("Rect { x: %f, y: %f, w: %f, h: %f }", rect.x, rect.y, rect.w, rect.h);
-                return os;
-            }
+        friend std::ostream& operator<<(std::ostream &os, const Rect &rect) {
+            printf("Rect { x: %f, y: %f, w: %f, h: %f }", rect.x, rect.y, rect.w, rect.h);
+            return os;
+        }
     };
 #endif
