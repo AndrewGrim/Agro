92f8b3c4df530f1213d89ec1ab4e4f2c5a54c98a
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-11-28 10:07:00 +0000

    Changed over to struct for color

--- src/common/color.hpp
+++ src/common/color.hpp
@@ -1,18 +1,17 @@
 #ifndef COLOR_HPP
     #define COLOR_HPP
 
-    class Color {
-        public:
-            float r;
-            float g;
-            float b;
-            float a;
+    struct Color {
+        float r;
+        float g;
+        float b;
+        float a;
 
-            Color(float r = 0.0f, float g = 0.0f, float b = 0.0f, float a = 1.0f) {
-                this->r = r;
-                this->g = g;
-                this->b = b;
-                this->a = a;
-            }
+        Color(float r = 0.0f, float g = 0.0f, float b = 0.0f, float a = 1.0f) {
+            this->r = r;
+            this->g = g;
+            this->b = b;
+            this->a = a;
+        }
     };
 #endif
