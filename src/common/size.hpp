71354442e6466a1ba39631d29e90c969089ee410
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-11-28 09:57:07 +0000

    Changed over size to float

--- src/common/size.hpp
+++ src/common/size.hpp
@@ -1,27 +1,26 @@
 #ifndef SIZE_HPP
     #define SIZE_HPP
 
-    template <class T> class Size {
-        public:
-            T w;
-            T h;
+    struct Size {
+        float w;
+        float h;
 
-            Size(T w = 0.0f, T h = 0.0f) {
-                this->w = w;
-                this->h = h;
+        Size(float w = 0.0f, float h = 0.0f) {
+            this->w = w;
+            this->h = h;
+        }
+
+        friend bool operator==(const Size &lhs, const Size &rhs) {
+            if (lhs.w == rhs.w && lhs.h == rhs.h) {
+                return true;
             }
 
-            friend bool operator==(const Size<T> &lhs, const Size<T> &rhs) {
-                if (lhs.w == rhs.w && lhs.h == rhs.h) {
-                    return true;
-                }
+            return false;
+        }
 
-                return false;
-            }
-
-            friend std::ostream& operator<<(std::ostream &os, const Size<T> &size) {
-                printf("Size { w: %f, h: %f }", size.w, size.h);
-                return os;
-            }
+        friend std::ostream& operator<<(std::ostream &os, const Size &size) {
+            printf("Size { w: %f, h: %f }", size.w, size.h);
+            return os;
+        }
     };
 #endif
