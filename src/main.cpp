1aba940005e0c9db6cee17d5f00f868c769ee8c5
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-11-29 20:10:27 +0000

    Update main for button with an onClick test

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <ctime>
 
 #include "util.hpp"
 #include "app.hpp"
 #include "controls/widget.hpp"
 #include "controls/box.hpp"
-// #include "controls/button.hpp"
+#include "controls/button.hpp"
 // #include "controls/slider.hpp"
 // #include "controls/scrollbar.hpp"
 // #include "controls/scrolledbox.hpp"
@@ -14,7 +15,14 @@
 
 int main() { 
     Application *app = new Application("Application", Size(500, 500));
-        app->mainWidget()->setBackground(Color(0.5, 0.2, 0.4, 0.8));
+    app->mainWidget()->setBackground(Color(0.5, 0.2, 0.4, 0.8));
+            Button *b = new Button("Button");
+                // TODO maybe rename to onClick instead??
+                b->onMouseClick = [&](MouseEvent event) {
+                    auto t = std::time(nullptr);
+                    b->setText(std::asctime(std::localtime(&t)));
+                };
+            app->append(b);
         // app->append(new LineEdit("text 1"), Fill::Horizontal);
         // app->append(new LineEdit("text 2"), Fill::Horizontal);
     app->run();
