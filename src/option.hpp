20c75e230ff5ee1ecb9dbb3212426dda2b464356
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-11-29 16:50:07 +0000

    Add missing header guards to Option

--- src/option.hpp
+++ src/option.hpp
@@ -1,29 +1,33 @@
-template <typename T> struct Option {
-    enum class Type {
-        Some,
-        None
-    };
+#ifndef OPTION_HPP
+    #define OPTION_HPP
     
-    Type type;
-    T value;
+    template <typename T> struct Option {
+        enum class Type {
+            Some,
+            None
+        };
+        
+        Type type;
+        T value;
 
-    Option(T value) {
-        this->type = Type::Some;
-        this->value = value;
-    }
-
-    Option() {
-        this->type = Type::None;
-    }
-
-    T unwrap() {
-        switch (this->type) {
-            case Type::Some:
-                return this->value;
-                break;
-            case Type::None:
-                assert(this->type == Type::Some);
-                break;
+        Option(T value) {
+            this->type = Type::Some;
+            this->value = value;
         }
-    }
-};
+
+        Option() {
+            this->type = Type::None;
+        }
+
+        T unwrap() {
+            switch (this->type) {
+                case Type::Some:
+                    return this->value;
+                    break;
+                case Type::None:
+                    assert(this->type == Type::Some);
+                    break;
+            }
+        }
+    };
+#endif
