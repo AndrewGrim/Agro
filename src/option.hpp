1c360079f8f8f81a6991e73764041de5d3e840da
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-11-29 11:09:05 +0000

    Added ctors for Option

--- src/option.hpp
+++ src/option.hpp
@@ -7,6 +7,15 @@ template <typename T> struct Option {
     Type type;
     T value;
 
+    Option(T value) {
+        this->type = Type::Some;
+        this->value = value;
+    }
+
+    Option() {
+        this->type = Type::None;
+    }
+
     T unwrap() {
         switch (this->type) {
             case Type::Some:
