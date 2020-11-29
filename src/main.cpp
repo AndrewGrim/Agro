945a92270776638585096f272ebf7461194580dd
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-11-29 18:52:37 +0000

    Added box include to main and change of color to test its  working...
    It is :^)

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include "util.hpp"
 #include "app.hpp"
 #include "controls/widget.hpp"
-// #include "controls/box.hpp"
+#include "controls/box.hpp"
 // #include "controls/button.hpp"
 // #include "controls/slider.hpp"
 // #include "controls/scrollbar.hpp"
@@ -14,6 +14,7 @@
 
 int main() { 
     Application *app = new Application("Application", Size(500, 500));
+        app->mainWidget()->setBackground(Color(0.5, 0.2, 0.4, 0.8));
         // app->append(new LineEdit("text 1"), Fill::Horizontal);
         // app->append(new LineEdit("text 2"), Fill::Horizontal);
     app->run();
