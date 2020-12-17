55422fc8a730974617665315ec1788d4874d26e6
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-12-17 11:10:50 +0000

    Add this qualifiers, dont flip, texture now works!

--- src/renderer/texture.hpp
+++ src/renderer/texture.hpp
@@ -18,7 +18,8 @@
 
         Texture(std::string file_path) {
             this->file_path = file_path;
-            stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
+            // TODO the below is not necessary for nergigante, how do we tell that it is needed??
+            // stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
             this->data = stbi_load(
                 file_path.c_str(), 
                 &this->width, 
@@ -27,20 +28,9 @@
                 0
             );
             if (this->data) {
-                // TODO this is likely part of the problem with images
-                // i assume that this overrides the currently assigned font texture
-                // what im thinking is:
-                // reserve the first 2 texture slots
-                // the first one for a white texture - this will be used to draw "normal" quads once we join the buffers into one
-                // the second one could be used for loading any random textures
-                // then instead of reseting the texture count to 0 just reset to 2 instead!
-
-                // TODO also we need to draw the image at the specified position
-                // and optionally at a different size
-                // and i notice that only quad renderer has a rect parameter
                 glActiveTexture(GL_TEXTURE0);
-                glGenTextures(1, &ID);
-                glBindTexture(GL_TEXTURE_2D, ID);
+                glGenTextures(1, &this->ID);
+                glBindTexture(GL_TEXTURE_2D, this->ID);
 
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
@@ -49,7 +39,7 @@
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
                 // println("nr_channels: " + std::to_string(nr_channels)); // this tells us if we need rgb or rgba for example
                 // TODO we might want to do GL_RGB for jpgs but GL_RGBA for pngs
-                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, this->data);
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this->width, this->height, 0, GL_RGBA, GL_UNSIGNED_BYTE, this->data);
                 glGenerateMipmap(GL_TEXTURE_2D);
             } else {
                 println("Error: Failed to load texture! '" + file_path + "'");
