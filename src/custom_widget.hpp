8652a9396e0a9e0e78a636dcc9beec10d15fad08
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-12-20 14:24:36 +0000

    Added text to the custom widget showcase

--- src/custom_widget.hpp
+++ src/custom_widget.hpp
@@ -26,6 +26,10 @@
                 dc->fillRect(rect, Color(0.7, 0.7, 0.7));
                 dc->fillRectWithGradient(Rect(rect.x, rect.y, 125, 100), Color(), Color(1, 0.1), Renderer::Gradient::LeftToRight);
                 dc->fillRectWithGradient(Rect(rect.x + 125, rect.y, 125, 100), Color(), Color(0.2, 0.7, 0.9), Renderer::Gradient::TopToBottom);
+                dc->fillTextAligned(dc->default_font, "Left Aligned Text", TextAlignment::Left, Rect(rect.x, rect.y, 250, 20), 10, Color(1, 1, 1));
+                dc->fillTextAligned(dc->default_font, "Centered Aligned Text", TextAlignment::Center, Rect(rect.x, rect.y, 250, 100), 10, Color(1, 1, 1));
+                dc->fillTextAligned(dc->default_font, "Right Aligned Text", TextAlignment::Right, Rect(rect.x, rect.y + 80, 250, 20), 10, Color(1, 1, 1));
+                dc->fillText(dc->default_font, "Teostra.png", Point(rect.x + 25, rect.y + 130));
                 dc->drawImageAtSize(Point(rect.x + 50, rect.y + 140), Size(24, 24), m_texture);
                 dc->drawImageAtSize(Point(rect.x + 125, rect.y + 90), Size(96, 96), m_texture);
                 dc->fillRect(Rect(rect.x, rect.y + 175, rect.w, 20), Color());
