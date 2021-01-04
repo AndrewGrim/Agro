0cba8726536c43697d13abccf1428cc1da389f57
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-01-04 14:37:19 +0000

    Changed header incoude, braces around if

--- src/util.hpp
+++ src/util.hpp
@@ -1,13 +1,16 @@
 #ifndef UTIL_HPP
     #define UTIL_HPP
-        #include <iostream>
+    
+        #include <ostream>
 
         template <typename T> void pprint(int indent, T message, bool newline = true) {
             for (int i = 0; i < indent; i++) {
                 std::cout << " ";
             }
             std::cout << message;
-            if (newline) std::cout << "\n";
+            if (newline) {
+                std::cout << "\n";
+            }
         }
 
         template <typename T> void print(T message) {
