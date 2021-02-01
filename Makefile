3626a091d02ee9139e8acab3ff3bbedf196e7b2d
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-02-01 15:47:52 +0000

    Added test to show off improvements in LineEdit

--- Makefile
+++ Makefile
@@ -91,3 +91,5 @@ test_scrolledbox_both:
 	$(CXX) tests/scrolledbox_both.cpp $(OBJECT_FILES) $(INCLUDE) $(LINK) $(LIBS) $(CXX_FLAGS) -o scrolledbox_both.out && ./scrolledbox_both.out
 test_scrolledbox_inception_clipping:
 	$(CXX) tests/scrolledbox_inception_clipping.cpp $(OBJECT_FILES) $(INCLUDE) $(LINK) $(LIBS) $(CXX_FLAGS) -o scrolledbox_inception_clipping.out && ./scrolledbox_inception_clipping.out
+test_lineedit_search:
+	$(CXX) tests/lineedit_search.cpp $(OBJECT_FILES) $(INCLUDE) $(LINK) $(LIBS) $(CXX_FLAGS) -o lineedit_search.out && ./lineedit_search.out
---
+++ tests/lineedit_search.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+
+#include "../src/util.hpp"
+#include "../src/app.hpp"
+#include "../src/controls/lineedit.hpp"
+#include "../src/controls/scrolledbox.hpp"
+#include "../src/controls/label.hpp"
+
+int main() { 
+    Application *app = new Application("ScrolledBox Inception Clipping Test", Size(400, 400));
+        std::vector<std::string> monster_names = {
+            "Great Jagras", "Kulu-Ya-Ku", "Pukei-Pukei", "Barroth",
+            "Jyuratodus", "Tobi-Kadachi", "Anjanath", "Rathian",
+            "Tzitzi-Ya-Ku", "Paolumu", "Great Girros", "Radobaan", 
+            "Legiana", "Odogaron", "Rathalos", "Diablos", "Kirin", 
+            "Zorah Magdaros", "Dodogama", "Pink Rathian", "Bazelgeuse", 
+            "Deviljho", "Lavasioth", "Uragaan", "Azure Rathalos", "Black Diablos", 
+            "Nergigante", "Teostra", "Lunastra", "Kushala Daora", "Vaal Hazak", 
+            "Kulve Taroth", "Xeno'jiiva", "Behemoth", "Leshen", "Ancient Leshen",
+        };
+        sort(monster_names.begin(), monster_names.end());
+        ScrolledBox *results_view;
+        LineEdit *edit = new LineEdit();
+            edit->setPlaceholderText("Search by name");
+            edit->onTextChanged = [&]{
+                while (results_view->children.size()) {
+                    results_view->children[0]->destroy();
+                }
+                for (auto m : monster_names) {
+                    auto compare = m;
+                    std::transform(compare.begin(), compare.end(), compare.begin(), [](unsigned char c){
+                        return std::tolower(c);
+                    });
+                    if (compare.find(edit->text()) != std::string::npos || !edit->text().size()) {
+                        results_view->append(new Label(m));
+                    }
+                }
+            };
+        app->append(edit, Fill::Horizontal);
+        results_view = new ScrolledBox(Align::Vertical);
+            for (auto m : monster_names) {
+                results_view->append(new Label(m));
+            }
+        app->append(results_view, Fill::Both);
+
+    #ifdef TEST
+        #include "headless.hpp"
+    #else
+        app->run();
+    #endif
+
+    return 0; 
+}
