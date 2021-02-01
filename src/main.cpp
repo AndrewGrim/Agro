6a8f12c3acc7c96c0e3da34fe67189fa9dd519ae
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-02-01 19:14:40 +0000

    Got rid off extra imports

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,9 @@
 
 #include "util.hpp"
 #include "app.hpp"
-#include "option.hpp"
-#include "controls/widget.hpp"
 #include "controls/box.hpp"
 #include "controls/button.hpp"
-#include "controls/image.hpp"
-#include "controls/label.hpp"
-#include "controls/slider.hpp"
-#include "controls/scrollbar.hpp"
-#include "controls/scrolledbox.hpp"
-#include "controls/lineedit.hpp"
 #include "controls/notebook.hpp"
-
-#include "custom_widget.hpp"
 
 int main(int argc, char **argv) { 
     Application *app = new Application("Application", Size(500, 500));
