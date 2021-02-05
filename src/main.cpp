2cd3d57f9d4c3ae2eaae79a359e1d77288baea3a
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-02-05 11:35:05 +0000

    Update main to use treeview

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,13 @@
 #include "controls/box.hpp"
 #include "controls/button.hpp"
 #include "controls/notebook.hpp"
+#include "controls/treeview.hpp"
 
 int main(int argc, char **argv) { 
     Application *app = new Application("Application", Size(500, 500));
-        NoteBook *nb = new NoteBook();
-        {
-            for (int t = 1; t < 15; t++) {
-                Box *_b = new Box(Align::Vertical);
-                    for (int i = 0; i < 5; i++) {
-                        _b->append((new Button(std::to_string(i)))->setBackground(Color(t / 100.0 * 5)), Fill::Both);
-                    }
-                nb->appendTab(_b, "Tab " + std::to_string(t));
-            }
-        }
-        app->append(nb, Fill::Both);
+        TestTree *tt = new TestTree();
+        app->append(tt, Fill::Both);
     app->run();
 
-    return 0; 
+    return 0;
 }
