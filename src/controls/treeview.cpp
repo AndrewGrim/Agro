8e757101aabda758cc53ddee0d06272882b5e498
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-02-06 15:35:28 +0000

    Only draw the visible part

--- src/controls/treeview.cpp
+++ src/controls/treeview.cpp
@@ -21,17 +21,21 @@ void TestTree::draw(DrawingContext *dc, Rect rect) {
     Point pos = automaticallyAddOrRemoveScrollBars(dc, rect, virtual_size);
     dc->fillRect(rect, Color(0.6, 0.0, 0.2));
     for (int i = 0; i < 270; i++) {
-        dc->fillTextAligned(
-            dc->default_font,
-            std::to_string(i) + ". The quick brown fox jumps over the lazy dog!",
-            HorizontalAlignment::Left,
-            VerticalAlignment::Center,
-            Rect(pos.x, pos.y, virtual_size.w, 28),
-            10,
-            Color()
-        );
+        if (pos.y + 28 > rect.y) {
+            dc->fillTextAligned(
+                dc->default_font,
+                std::to_string(i) + ". The quick brown fox jumps over the lazy dog!",
+                HorizontalAlignment::Left,
+                VerticalAlignment::Center,
+                Rect(pos.x, pos.y, virtual_size.w, 28),
+                10,
+                Color()
+            );
+            if (pos.y + 28 > rect.y + rect.h) {
+                break;
+            }
+        }
         pos.y += 28;
-        // TODO only paints whats currently visible
     }
     drawScrollBars(dc, rect, virtual_size);
     dc->setClip(old_clip);
