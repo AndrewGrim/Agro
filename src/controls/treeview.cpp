e43e9921b5375d1f42ab4519b128f8007539e9cd
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-02-06 20:01:22 +0000

    Using buttons as column headers within treeview

--- src/controls/treeview.cpp
+++ src/controls/treeview.cpp
@@ -15,13 +15,31 @@ const char* TestTree::name() {
 void TestTree::draw(DrawingContext *dc, Rect rect) {
     this->rect = rect;
     Rect old_clip = dc->clip();
-    dc->setClip(rect);
-    Size virtual_size = Size(1000, 270 * 28);
+    Size children_size = Size();
+    for (Widget *child : children) {
+        Size s = child->sizeHint(dc);
+        children_size.w += s.w;
+        if (s.h > children_size.h) {
+            children_size.h = s.h;
+        }
+    }
+    Size virtual_size = Size(children_size.w, 270 * 28);
     dc->fillRect(rect, Color(0.6, 1.0, 0.2));
     Point pos = automaticallyAddOrRemoveScrollBars(dc, rect, virtual_size);
     dc->fillRect(rect, Color(0.6, 0.0, 0.2));
+    Rect local = rect;
+    float local_pos_x = pos.x;
+    dc->setClip(local);
+    for (Widget *child : children) {
+        Size s = child->sizeHint(dc);
+        child->draw(dc, Rect(local_pos_x, local.y, s.w, children_size.h));
+        local_pos_x += s.w;
+    }
+    local.y += children_size.h;
+    local.h -= children_size.h;
+    dc->setClip(Rect(rect.x, local.y, rect.w, local.h));
     for (int i = 0; i < 270; i++) {
-        if (pos.y + 28 > rect.y) {
+        if (pos.y + 28 > local.y) {
             dc->fillTextAligned(
                 dc->default_font,
                 std::to_string(i) + ". The quick brown fox jumps over the lazy dog!",
@@ -31,12 +49,12 @@ void TestTree::draw(DrawingContext *dc, Rect rect) {
                 10,
                 Color()
             );
-            if (pos.y + 28 > rect.y + rect.h) {
+            if (pos.y + 28 > local.y + local.h) {
                 break;
             }
         }
         pos.y += 28;
     }
-    drawScrollBars(dc, rect, virtual_size);
     dc->setClip(old_clip);
+    drawScrollBars(dc, rect, virtual_size);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,17 @@
 int main(int argc, char **argv) { 
     Application *app = new Application("Application", Size(500, 500));
         app->append(new Button("Top"), Fill::Horizontal);
+        app->bind(SDLK_q, Mod::Ctrl, [&]{
+            app->quit();
+        });
         Box *box = new Box(Align::Horizontal);
             box->append(new Button("Left"), Fill::Vertical);
             TestTree *tt = new TestTree();
+            {
+                for (int i = 0; i < 5; i++) {
+                    tt->append(new Button("Header: Col. " + std::to_string(i)));
+                }
+            }
             box->append(tt, Fill::Both);
         app->append(box, Fill::Both);
     app->run();
