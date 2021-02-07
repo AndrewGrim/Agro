21527143463634e26dd6a6700265cffa3cfecb22
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-02-07 19:37:12 +0000

    Clip each of the columns while drawing

--- src/controls/treeview.hpp
+++ src/controls/treeview.hpp
@@ -261,7 +261,6 @@
                         float cell_start = pos.x;
                         float row_height = 0.0;
                         for (size_t i = 0; i < node->columns.size(); i++) {
-                            // TODO need to clip each column
                             // TODO because we dont query the size beforehand yet
                             // if the text in a cell is longer than the column header width
                             // the cell will center text incorrectly
@@ -269,6 +268,13 @@
                             Size s = renderer->sizeHint(dc);
                             if (s.h > row_height) {
                                 row_height = s.h; // TODO we should check that in advance so we can give each cell more space when possible
+                            }
+                            if (cell_start + column_widths[i] > rect.x) {
+                                if (cell_start > rect.x) {
+                                    dc->setClip(Rect(cell_start, rect.y + children_size.h, column_widths[i], rect.h - children_size.h));
+                                } else {
+                                    dc->setClip(Rect(rect.x, rect.y + children_size.h, cell_start + column_widths[i] - rect.x, rect.h - children_size.h));
+                                }
                             }
                             renderer->draw(dc, Rect(cell_start, pos.y, column_widths[i], s.h));
                             cell_start += column_widths[i];
