1165681cea574c13236797de44745a21798003c9
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-02-24 19:47:43 +0000

    Added texture ctor to create texture from memory

--- src/renderer/texture.hpp
+++ src/renderer/texture.hpp
@@ -52,6 +52,40 @@
             }
         }
 
+        Texture(bool from_memory, const unsigned char *image_data, int length) {
+            assert(image_data && "Null image data!");
+            int nr_channels = -1;
+            unsigned char *data = stbi_load_from_memory(
+                image_data, 
+                length, 
+                &width, 
+                &height, 
+                &nr_channels, 
+                0
+            );
+            if (data) {
+                glActiveTexture(GL_TEXTURE0);
+                glGenTextures(1, &ID);
+                glBindTexture(GL_TEXTURE_2D, ID);
+
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+                assert((nr_channels == 3 || nr_channels == 4) && "Unsupported number of channels!");
+                if (nr_channels == 4) {
+                    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+                } else {
+                    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+                }
+                glGenerateMipmap(GL_TEXTURE_2D);
+                stbi_image_free(data);
+            } else {
+                println("Error: Failed to load texture from memory!");
+            }
+        }
+
         ~Texture() {
             glDeleteTextures(1, &this->ID);
         }
