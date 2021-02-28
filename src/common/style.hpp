e3a6295613e69cb471f7f223a45339a8e6775ee1
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-02-28 12:52:01 +0000

    Change default style members

--- src/common/style.hpp
+++ src/common/style.hpp
@@ -23,27 +23,27 @@
     // i pressume anyway, the uint32_t method should also be easier
     // to work with rgba int and hex | string
     struct Style {
-        int margin = STYLE_ALL;
-        int margin_top = 0;
-        int margin_bottom = 0;
-        int margin_left = 0;
-        int margin_right = 0;
+        int margin = STYLE_DEFAULT;
+        int margin_top = STYLE_DEFAULT;
+        int margin_bottom = STYLE_DEFAULT;
+        int margin_left = STYLE_DEFAULT;
+        int margin_right = STYLE_DEFAULT;
 
-        int border = STYLE_ALL;
-        int border_top = 2;
+        int border = STYLE_DEFAULT;
+        int border_top = STYLE_DEFAULT;
         Color border_top_color = COLOR_BLACK;
-        int border_bottom = 2;
+        int border_bottom = STYLE_DEFAULT;
         Color border_bottom_color = COLOR_BLACK;
-        int border_left = 2;
+        int border_left = STYLE_DEFAULT;
         Color border_left_color = COLOR_BLACK;
-        int border_right = 2;
+        int border_right = STYLE_DEFAULT;
         Color border_right_color = COLOR_BLACK;
 
-        int padding = STYLE_ALL;
-        int padding_top = 5;
-        int padding_bottom = 5;
-        int padding_left = 5;
-        int padding_right = 5;
+        int padding = STYLE_DEFAULT;
+        int padding_top = STYLE_DEFAULT;
+        int padding_bottom = STYLE_DEFAULT;
+        int padding_left = STYLE_DEFAULT;
+        int padding_right = STYLE_DEFAULT;
 
         Color text_color = COLOR_BLACK;
         Color background_color = Color(0.9, 0.9, 0.9);
