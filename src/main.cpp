6d0f66a3e72f8c6f2b07cd3852f518b4ccd4d27f
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-04-07 12:29:25 +0100

    Update main to test sliders

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,16 @@
 #include "util.hpp"
 #include "application.hpp"
-#include "controls/treeview.hpp"
+#include "controls/slider.hpp"
 
 int main(int argc, char **argv) { 
     Application *app = Application::get();
-        app->resize(400, 800);
-        TreeView<void> *tv = new TreeView<void>();
-            tv->append(new Column<void>("Testing treelines at more than 1 depth!"));
-            tv->children[0]->tooltip = "Testing";
-            Tree<void> *model = new Tree<void>();
-                for (int n = 0; n < 2; n++) {
-                    TreeNode<void> *parent = nullptr;
-                    for (int i = 0; i < 5; i++) {
-                        std::vector<CellRenderer*> columns = { new TextCellRenderer(std::to_string(i) + ":1") };
-                        TreeNode<void> *node = new TreeNode<void>(columns, nullptr);
-                        parent = model->append(parent, node);
-                        {
-                            std::vector<CellRenderer*> columns = { new TextCellRenderer(std::to_string(i) + ":2") };
-                            TreeNode<void> *node = new TreeNode<void>(columns, nullptr);
-                            model->append(parent->parent, node);
-                        }
-                    }
-                }
-            tv->setModel(model);
-        app->append(tv, Fill::Both);
+        app->resize(400, 400);
+        auto h = new Slider(Align::Horizontal);
+            h->m_slider_button_size = 50;
+        app->append(h, Fill::Horizontal);
+        auto v = new Slider(Align::Vertical);
+            v->m_slider_button_size = 50;
+        app->append(v, Fill::Vertical);
     app->run();
 
     return 0;
