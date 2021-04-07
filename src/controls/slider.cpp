6ad4138267f91aafe3976b8aa882f6d20ead2f70
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-04-07 12:50:55 +0100

    Rewrite of slider...
    The feel of the slider has been greatly improved.
    Instead of just using relative mouse movement
    we use a combination of mouse position and mouse movement, and
    we switch between the two based on where the mouse is.
    The slider value now comes from the middle of the slider button, and
    because of it the slider behaves very similiar to what people are generally used
    to (like vscode for example).
    The only slight issue is that when dragging the slider on the far end on either side
    when the mouse gets to the min / max values the slider will snap to that value a little.

--- src/controls/slider.cpp
+++ src/controls/slider.cpp
@@ -2,9 +2,9 @@
 #include "widget.hpp"
 #include "slider.hpp"
 
-SliderButton::SliderButton() : Widget() {
+SliderButton::SliderButton(Size min_size) : Widget() {
     Widget::m_bg = Color(0.9f, 0.9f, 0.9f);
-    setMinSize(Size(20, 20));
+    setMinSize(min_size);
 }
 
 SliderButton::~SliderButton() {
@@ -44,47 +44,62 @@ Size SliderButton::minSize() {
     return m_size;
 }
 
-Slider::Slider(Align alignment, float value) : Box(alignment) {
+Slider::Slider(Align alignment, float value) : Box(alignment), m_value{value} {
     Widget::m_bg = COLOR_BLACK;
-    this->m_value = value;
-    this->m_slider_button = new SliderButton();
-    if (alignment == Align::Horizontal) {
-        // TODO investigate, because scrollbar does something different
-        this->append(this->m_slider_button, Fill::Horizontal);
-    } else {
-        this->append(this->m_slider_button, Fill::Vertical);
-    }
-    this->m_slider_button->parent = this;
-    this->m_slider_button->onMouseMotion.addEventListener([&](Widget *widget, MouseEvent event) {
-        SliderButton *self = this->m_slider_button;
-        Rect rect = this->rect;
+    m_slider_button = new SliderButton();
+    append(m_slider_button, Fill::Both);
+    m_slider_button->parent = this;
+
+    m_slider_button->onMouseMotion.addEventListener([&](Widget *widget, MouseEvent event) {
+        SliderButton *self = m_slider_button;
         if (self->isPressed()) {
-            // TODO probably should still switch to mouse x and y
-            // just make it so it only moves from the center of the slider
-            // then we might be able to go back to the old limiter in the main loop
-            // which would be good for controlling the framerate
-            if (this->m_align_policy == Align::Horizontal) {
-                float value = this->m_value + (event.xrel / (rect.w - this->m_slider_button_size));
-                if (value > this->m_max) {
-                    value = this->m_max;
-                } else if (value < this->m_min) {
-                    value = this->m_min;
-                }
-                this->m_value = value;
+            Rect rect = this->rect;
+            float size = m_slider_button_size;
+
+            int event_pos;
+            int event_pos_rel;
+            float position;
+            float length;
+            if (m_align_policy == Align::Horizontal) {
+                event_pos = event.x;
+                event_pos_rel = event.xrel;
+                position = rect.x;
+                length = rect.w;
             } else {
-                float value = this->m_value + (event.yrel / (rect.h - this->m_slider_button_size));
-                if (value > this->m_max) {
-                    value = this->m_max;
-                } else if (value < this->m_min) {
-                    value = this->m_min;
-                }
-                this->m_value = value;
+                event_pos = event.y;
+                event_pos_rel = event.yrel;
+                position = rect.y;
+                length = rect.h;
             }
-            if (this->onValueChanged) {
-                this->onValueChanged();
+
+            // When the mouse position is outside the value axis use
+            // the mouse position from the event to calculate the value.
+            if ((event_pos < position + (size / 2)) || (event_pos > (position + (size / 2)) + (length - size))) {
+                m_value = (event_pos - (position + (size / 2))) / (length - size);
+            // When the mouse position is inside the value axis use
+            // the relative mouse movement from the event to calculate the value.
+            } else {
+                m_value = m_value + (event_pos_rel / (length - size));
             }
-            this->update();
+            // Normalize the value.
+            m_value = m_value < m_min ? m_min : m_value > m_max ? m_max : m_value;
+        
+            if (onValueChanged) {
+                onValueChanged();
+            }
+            update();
         }
+    });
+
+    this->onMouseDown.addEventListener([&](Widget *widget, MouseEvent event) {
+        Rect rect = this->rect;
+        float size = m_slider_button_size;
+        if (m_align_policy == Align::Horizontal) {
+            m_value = (event.x - (rect.x + size / 2)) / (rect.w - size);
+        } else {
+            m_value = (event.y - (rect.y + size / 2)) / (rect.h - size);
+        }
+        m_value = m_value < m_min ? m_min : m_value > m_max ? m_max : m_value;
     });
 }
 
@@ -99,85 +114,79 @@ const char* Slider::name() {
 
 void Slider::draw(DrawingContext *dc, Rect rect) {
     this->rect = rect;
-    if (this->m_align_policy == Align::Horizontal) {
-        dc->fillRect(Rect(rect.x, rect.y + rect.h / 2, rect.w, 2), this->background());
-    } else {
-        dc->fillRect(Rect(rect.x + rect.w / 2, rect.y, 2, rect.h), this->background());
-    }
-    Size sizehint = this->m_slider_button->sizeHint(dc);
+
+    // Get the size of the slider button.
     float size;
-    if (!this->m_slider_button_size) {
-        if (this->m_align_policy == Align::Horizontal) {
+    Size sizehint = m_slider_button->sizeHint(dc);
+    if (!m_slider_button_size) {
+        // Button size was not set. Default.
+        if (m_align_policy == Align::Horizontal) {
             size = sizehint.w;
         } else {
             size = sizehint.h;
         }
     } else {
-        size = this->m_slider_button_size;
-    }
-    float *align_rect[2] = {};
-    if (this->m_align_policy == Align::Horizontal) {
-        align_rect[0] = &rect.x;
-        align_rect[1] = &rect.w;
-    } else {
-        align_rect[0] = &rect.y;
-        align_rect[1] = &rect.h;
+        // Button size was set externally, usually by a scrollable widget.
+        size = m_slider_button_size;
     }
 
-    float result = ((*align_rect[1] - size) * this->m_value);
-    if (result <= 0) {
-        // NO OP
-    } else if (result > (*align_rect[1] - size)) {
-        *align_rect[0] += (*align_rect[1] - size);
+    // Draw the "rail" for the slider button.
+    if (m_align_policy == Align::Horizontal) {
+        dc->fillRect(Rect(rect.x + (size / 2), rect.y + rect.h / 2, rect.w - size, 2), background());
     } else {
-        *align_rect[0] += result;
+        dc->fillRect(Rect(rect.x + rect.w / 2, rect.y + (size / 2), 2, rect.h - size), background());
     }
-    if (this->m_align_policy == Align::Horizontal) {
-        this->m_slider_button->draw(dc, Rect(rect.x, rect.y, size, rect.h));
+
+    // Determine and draw the location of the slider button.
+    if (m_align_policy == Align::Horizontal) {
+        float result = ((rect.w - size) * m_value);
+        if (result > 0) {
+            rect.x += result;
+        }
+        m_slider_button->draw(dc, Rect(rect.x, rect.y, size, rect.h));
     } else {
-        this->m_slider_button->draw(dc, Rect(rect.x, rect.y, rect.w, size));
+        float result = ((rect.h - size) * m_value);
+        if (result > 0) {
+            rect.y += result;
+        }
+        m_slider_button->draw(dc, Rect(rect.x, rect.y, rect.w, size));
     }
 }
 
 Size Slider::sizeHint(DrawingContext *dc) {
     Size size = this->m_slider_button->sizeHint(dc);
     if (this->m_align_policy == Align::Horizontal) {
-        size.w *= 2;
+        // The 3 is because we want the slider to be
+        // at least 3 buttons wide
+        // 2 so that the button can move on its axis by its full length
+        // and 1 because the value comes from the center of the button
+        // so we need half the width of the button on either side
+        // so that the min and max values are where theyre supposed to be.
+        // ++--------++
+        // ====------++
+        size.w *= 3;
     } else {
-        size.h *= 2;
+        size.h *= 3;
     }
 
     return size;
 }
 
-Slider* Slider::setForeground(Color foreground) {
-    // The foreground color for this widget acutally affects
-    // the slider button.
-    m_slider_button->setBackground(foreground);
-    this->update();
-
-    return this;
-}
-
-Slider* Slider::setBackground(Color background) {
-    // The background color for this widget only affects
-    // the line on which the slider sits on.
-    Widget::m_bg = background;
-    this->update();
-
-    return this;
-}
+#define SCROLL_AMOUNT 50
 
 bool Slider::handleScrollEvent(ScrollEvent event) {
     // TODO should we do this automatically in ScrollEvent() ctor?
     event.y *= -1;
     if (this->m_align_policy == Align::Horizontal) {
-        float value = this->m_value + (event.y / (rect.w - this->m_slider_button_size));
+        // TODO the right calculation but on the wrong values
+        // instead of rect.w - button_size
+        // it needs to be on the total length of the scrollable area
+        float value = this->m_value + ((SCROLL_AMOUNT * event.y) / (rect.w - m_slider_button_size)); //  + (event.y / 100.0);//(event.y / (rect.w - this->m_slider_button_size));
         if (value > this->m_max) value = this->m_max;
         else if (value < this->m_min) value = this->m_min;
         this->m_value = value;
     } else {
-        float value = this->m_value + (event.y / (rect.h - this->m_slider_button_size));
+        float value = this->m_value + ((SCROLL_AMOUNT * event.y) / (rect.h - m_slider_button_size)); // + (event.y / 100.0);// (event.y / (rect.h - this->m_slider_button_size));
         if (value > this->m_max) value = this->m_max;
         else if (value < this->m_min) value = this->m_min;
         this->m_value = value;
--- src/controls/slider.hpp
+++ src/controls/slider.hpp
@@ -8,7 +8,7 @@
 
     class SliderButton : public Widget {
         public:
-            SliderButton();
+            SliderButton(Size min_size = Size(10, 10));
             ~SliderButton();
             virtual const char* name() override;
             virtual void draw(DrawingContext *dc, Rect rect) override;
@@ -21,18 +21,18 @@
         public:
             float m_min = 0.0f;
             float m_max = 1.0f;
-            float m_value;
+            float m_value = 0.0f;
+            float m_step = 0.1f; 
+            
             float m_slider_button_size = 0;
             SliderButton *m_slider_button = nullptr;
             std::function<void(void)> onValueChanged = nullptr;
 
-            Slider(Align alignment, float value = 0.0);
+            Slider(Align alignment, float value = 0.0f);
             ~Slider();
             virtual const char* name() override;
             virtual void draw(DrawingContext *dc, Rect rect) override;
             virtual Size sizeHint(DrawingContext *dc) override;
             virtual bool handleScrollEvent(ScrollEvent event) override;
-            virtual Slider* setBackground(Color background) override;
-            virtual Slider* setForeground(Color foreground) override;
     };
 #endif
