57b8a9b25028a3a181adb2fe3f40bedae37416fa
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-05-08 15:58:20 +0100

    Removed extra this, font is checked when drawing text anyway

--- src/controls/label.cpp
+++ src/controls/label.cpp
@@ -1,7 +1,7 @@
 #include "label.hpp"
 
 Label::Label(std::string text) {
-    this->setText(text);
+    setText(text);
     Widget::m_bg = COLOR_NONE;
 }
 
@@ -31,45 +31,45 @@ void Label::draw(DrawingContext *dc, Rect rect) {
     dc->fillRect(drawing_rect, Widget::m_bg);
     dc->padding(drawing_rect, style);
     dc->fillTextAligned(
-        this->font() ? this->font() : dc->default_font,
-        this->m_text,
-        this->m_horizontal_align,
-        this->m_vertical_align,
+        font(),
+        m_text,
+        m_horizontal_align,
+        m_vertical_align,
         drawing_rect,
         0,
-        this->m_fg
+        m_fg
     );
 }
 
 Size Label::sizeHint(DrawingContext *dc) {
-    if (this->m_size_changed) {
-        Size size = dc->measureText(this->font() ? this->font() : dc->default_font, this->text());
+    if (m_size_changed) {
+        Size size = dc->measureText(font(), text());
         dc->sizeHintPadding(size, style);
-        this->m_size = size;
-        this->m_size_changed = false;
+        m_size = size;
+        m_size_changed = false;
 
         return size;
     } else {
-        return this->m_size;
+        return m_size;
     }
 }
 
 Label* Label::setForeground(Color foreground) {
     Widget::m_fg = foreground;
-    this->update();
+    update();
 
     return this;
 }
 
 Label* Label::setBackground(Color background) {
     Widget::m_bg = background;
-    this->update();
+    update();
 
     return this;
 }
 
 std::string Label::text() {
-    return this->m_text;
+    return m_text;
 }
 
 Label* Label::setText(std::string text) {
@@ -78,33 +78,33 @@ Label* Label::setText(std::string text) {
     // that can be pretty expensive and we can
     // assume that if someone is setting the text
     // its going to be different.
-    this->m_text = text;
-    this->layout();
+    m_text = text;
+    layout();
 
     return this;
 }
 
 HorizontalAlignment Label::horizontalAlignment() {
-    return this->m_horizontal_align;
+    return m_horizontal_align;
 }
 
 Label* Label::setHorizontalAlignment(HorizontalAlignment text_align) {
     if (m_horizontal_align != text_align) {
         m_horizontal_align = text_align;
-        this->update();
+        update();
     }
 
     return this;
 }
 
 VerticalAlignment Label::verticalAlignment() {
-    return this->m_vertical_align;
+    return m_vertical_align;
 }
 
 Label* Label::setVerticalAlignment(VerticalAlignment text_align) {
     if (m_vertical_align != text_align) {
         m_vertical_align = text_align;
-        this->update();
+        update();
     }
 
     return this;
