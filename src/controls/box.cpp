ea8b321a47cff30da7c58dae507387af777e7b24
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-05-17 20:42:37 +0100

    Changed to if, its clearer that all cases are handled

--- src/controls/box.cpp
+++ src/controls/box.cpp
@@ -31,27 +31,24 @@ void Box::layoutChildren(DrawingContext *dc, Rect rect) {
     float *generic_length; // Needs to be a ptr because the value will change.
     Size app_size = Application::get()->size;
     float generic_app_length;
-    switch (parent_layout) {
-        case Align::Vertical:
-            generic_non_expandable_widgets = this->m_vertical_non_expandable;
-            generic_total_layout_length = this->m_size.h;
-            generic_max_layout_length = this->m_size.w;
-            generic_position_coord = &pos.y;
-            rect_length = rect.h;
-            rect_opposite_length = rect.w;
-            generic_length = &size.h;
-            generic_app_length = app_size.h;
-            break;
-        case Align::Horizontal:
-            generic_non_expandable_widgets = this->m_horizontal_non_expandable;
-            generic_total_layout_length = this->m_size.w;
-            generic_max_layout_length = this->m_size.h;
-            generic_position_coord = &pos.x;
-            rect_length = rect.w;
-            rect_opposite_length = rect.h;
-            generic_length = &size.w;
-            generic_app_length = app_size.w;
-            break;
+    if (parent_layout == Align::Vertical) {
+        generic_non_expandable_widgets = this->m_vertical_non_expandable;
+        generic_total_layout_length = this->m_size.h;
+        generic_max_layout_length = this->m_size.w;
+        generic_position_coord = &pos.y;
+        rect_length = rect.h;
+        rect_opposite_length = rect.w;
+        generic_length = &size.h;
+        generic_app_length = app_size.h;
+    } else {
+        generic_non_expandable_widgets = this->m_horizontal_non_expandable;
+        generic_total_layout_length = this->m_size.w;
+        generic_max_layout_length = this->m_size.h;
+        generic_position_coord = &pos.x;
+        rect_length = rect.w;
+        rect_opposite_length = rect.h;
+        generic_length = &size.w;
+        generic_app_length = app_size.w;
     }
     int child_count = this->m_visible_children - generic_non_expandable_widgets;
     if (!child_count) child_count = 1; // Protects from division by zero
