d588db3025cd2dde686a744de361e97a80cde42d
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-05-17 21:45:01 +0100

    Small change in indentation

--- embed_resource.py
+++ embed_resource.py
@@ -1,14 +1,15 @@
 def create_resource_bytes(filename):
     with open(filename, "rb") as f:
+        tab = " " * 4
         normalized_filename = filename.replace(" ", "_").replace(".", "_")
-        print(f"unsigned char {normalized_filename}[] = {{\n\t", end="")
+        print(f"unsigned char {normalized_filename}[] = {{\n{tab}", end="")
         length = 0
         for line in f:
             for byte in line:
                 print(f"0x{byte:02X}", end=", ")
                 length += 1
                 if length % 12 == 0:
-                    print("\n\t", end="")
+                    print(f"\n{tab}", end="")
         print("\n};")
         print(f"unsigned int {normalized_filename}_length = {length};")
 
