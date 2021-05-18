6537cfc85d9dde0da8eb14dc9fbdda68d30c482a
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-05-18 09:58:40 +0100

    Update resource files

--- src/resources.hpp
+++ src/resources.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
 extern const unsigned char close_png[];
-extern const unsigned char close_png_length;
+extern const unsigned int close_png_length;
 
 extern const unsigned char close_thin_png[];
-extern const unsigned char close_thin_png_length;
+extern const unsigned int close_thin_png_length;
 
 extern const unsigned char up_arrow_png[];
-extern const unsigned char up_arrow_png_length;
+extern const unsigned int up_arrow_png_length;
 
