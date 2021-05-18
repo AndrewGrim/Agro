60f1a575485cdb00c01e83db65957c82010acd8a
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-05-18 21:23:21 +0100

    Update embeds...
    Only changed because we use os.listdir instead of manually specifying files.

--- src/resources.hpp
+++ src/resources.hpp
@@ -3,9 +3,9 @@
 extern const unsigned char close_png[];
 extern const unsigned int close_png_length;
 
-extern const unsigned char close_thin_png[];
-extern const unsigned int close_thin_png_length;
-
 extern const unsigned char up_arrow_png[];
 extern const unsigned int up_arrow_png_length;
+
+extern const unsigned char close_thin_png[];
+extern const unsigned int close_thin_png_length;
 
