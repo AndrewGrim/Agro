a57c746a7b58606d262eeb9734897f519ea7c7bc
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-05-18 09:37:24 +0100

    Add files which contain the embedded resources

---
+++ src/resources.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+extern const unsigned char close_png[];
+extern const unsigned char close_png_length;
+
+extern const unsigned char close_thin_png[];
+extern const unsigned char close_thin_png_length;
+
+extern const unsigned char up_arrow_png[];
+extern const unsigned char up_arrow_png_length;
+
