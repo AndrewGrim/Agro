9129ef5aa02a6bed02a3400d8724a18cdd34f013
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-05-18 09:36:56 +0100

    Modify script to output a cpp and hpp files for embedding resources

--- embed_resource.py
+++ embed_resource.py
@@ -1,22 +1,38 @@
+def normalize_filename(filename):
+    return filename.replace(" ", "_").replace(".", "_")
+
+
 def create_resource_bytes(filename):
+    string = ""
     with open(filename, "rb") as f:
         tab = " " * 4
-        normalized_filename = filename.replace(" ", "_").replace(".", "_")
-        print(f"unsigned char {normalized_filename}[] = {{\n{tab}", end="")
+        string += f"const unsigned char {normalize_filename(filename)}[] = {{\n{tab}"
         length = 0
         for line in f:
             for byte in line:
-                print(f"0x{byte:02X}", end=", ")
+                string += f"0x{byte:02X}, "
                 length += 1
                 if length % 12 == 0:
-                    print(f"\n{tab}", end="")
-        print("\n};")
-        print(f"unsigned int {normalized_filename}_length = {length};\n")
+                    string += f"\n{tab}"
+        string += "\n};\n"
+        string += f"const unsigned int {normalize_filename(filename)}_length = {length};\n\n"
+    return string
+
 
 def main():
     images = ["close.png", "close_thin.png", "up_arrow.png"]
+    cpp = open("src/resources.cpp", "w")
+    hpp = open("src/resources.hpp", "w")
+    hpp.write("#pragma once\n\n")
+
     for img in images:
-        create_resource_bytes(img)
+        cpp.write(create_resource_bytes(img))
+        hpp.write(f"extern const unsigned char {normalize_filename(img)}[];\n")
+        hpp.write(f"extern const unsigned char {normalize_filename(img)}_length;\n\n")
+
+    cpp.close()
+    hpp.close()
+
 
 if __name__ == "__main__":
     main()
