20e23b51acfca59602860c31dd3f17ccbe80f8c5
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-05-22 20:32:00 +0100

    Add dejavusans font to resources

--- src/resources.hpp
+++ src/resources.hpp
@@ -3,9 +3,12 @@
 extern const unsigned char close_png[];
 extern const unsigned int close_png_length;
 
+extern const unsigned char close_thin_png[];
+extern const unsigned int close_thin_png_length;
+
 extern const unsigned char up_arrow_png[];
 extern const unsigned int up_arrow_png_length;
 
-extern const unsigned char close_thin_png[];
-extern const unsigned int close_thin_png_length;
+extern const unsigned char DejaVuSans_ttf[];
+extern const unsigned int DejaVuSans_ttf_length;
 
