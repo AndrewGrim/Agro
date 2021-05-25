0f8d9efd6b2316a874038b7db7054439bc2c99d0
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-05-25 16:49:12 +0100

    Adjust tests to account for changes to color

--- tests/color.cpp
+++ tests/color.cpp
@@ -4,25 +4,25 @@
 #include "../src/common/color.hpp"
 
 int main(int argc, char **argv) {
-    assert(Color("") == Color::fromInt(0, 0, 0, 0));
-    assert(Color("!") == Color::fromInt(0, 0, 0, 0));
-    assert(Color("f") == Color::fromInt(240, 0, 0, 0));
-    assert(Color("ff") == Color::fromInt(255, 0, 0, 0));
-    assert(Color("ff9") == Color::fromInt(255, 144, 0, 0));
-    assert(Color("ff9b") == Color::fromInt(255, 155, 0, 0));
-    assert(Color("ff9b0") == Color::fromInt(255, 155, 0, 0));
-    assert(Color("ff9b00") == Color::fromInt(255, 155, 0, 0));
+    assert(Color("") == Color::fromInt(0, 0, 0, 255));
+    assert(Color("!") == Color::fromInt(0, 0, 0, 255));
+    assert(Color("f") == Color::fromInt(240, 0, 0, 255));
+    assert(Color("ff") == Color::fromInt(255, 0, 0, 255));
+    assert(Color("ff9") == Color::fromInt(255, 144, 0, 255));
+    assert(Color("ff9b") == Color::fromInt(255, 155, 0, 255));
+    assert(Color("ff9b0") == Color::fromInt(255, 155, 0, 255));
+    assert(Color("ff9b00") == Color::fromInt(255, 155, 0, 255));
     assert(Color("ff9b00f") == Color::fromInt(255, 155, 0, 240));
     assert(Color("ff9b00ff") == Color::fromInt(255, 155, 0, 255));
     assert(Color("ff9b00ff123uisndof") == Color::fromInt(255, 155, 0, 255));
-    assert(Color("#") == Color::fromInt(0, 0, 0, 0));
-    assert(Color("#!") == Color::fromInt(0, 0, 0, 0));
-    assert(Color("#f") == Color::fromInt(240, 0, 0, 0));
-    assert(Color("#ff") == Color::fromInt(255, 0, 0, 0));
-    assert(Color("#ff9") == Color::fromInt(255, 144, 0, 0));
-    assert(Color("#ff9b") == Color::fromInt(255, 155, 0, 0));
-    assert(Color("#ff9b0") == Color::fromInt(255, 155, 0, 0));
-    assert(Color("#ff9b00") == Color::fromInt(255, 155, 0, 0));
+    assert(Color("#") == Color::fromInt(0, 0, 0, 255));
+    assert(Color("#!") == Color::fromInt(0, 0, 0, 255));
+    assert(Color("#f") == Color::fromInt(240, 0, 0, 255));
+    assert(Color("#ff") == Color::fromInt(255, 0, 0, 255));
+    assert(Color("#ff9") == Color::fromInt(255, 144, 0, 255));
+    assert(Color("#ff9b") == Color::fromInt(255, 155, 0, 255));
+    assert(Color("#ff9b0") == Color::fromInt(255, 155, 0, 255));
+    assert(Color("#ff9b00") == Color::fromInt(255, 155, 0, 255));
     assert(Color("#ff9b00f") == Color::fromInt(255, 155, 0, 240));
     assert(Color("#ff9b00ff") == Color::fromInt(255, 155, 0, 255));
     assert(Color("#ff9b00ff123uisndof") == Color::fromInt(255, 155, 0, 255));
