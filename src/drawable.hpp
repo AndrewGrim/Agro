1323544c5f1842b28f1bd3060817735269433efc
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-06-02 22:19:28 +0100

    Move state from CellRenderer to Drawable

--- src/drawable.hpp
+++ src/drawable.hpp
@@ -4,9 +4,15 @@
 
 class Drawable {
 	public:
+		enum DrawableState {
+            STATE_DEFAULT = 0x0000,
+            STATE_HOVERED = 0x0010,
+            STATE_PRESSED = 0x0100,
+            STATE_FOCUSED = 0x1000,
+        };
 		Drawable() {}
 		virtual ~Drawable() {}
-		virtual void draw(DrawingContext &dc, Rect rect) = 0;
+		virtual void draw(DrawingContext &dc, Rect rect, int state) = 0;
 		virtual Size sizeHint(DrawingContext &dc) = 0;
 		virtual bool isWidget() = 0;
 };
