301446e78f6baf96da004e8cad83fd29df47f9d5
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-06-02 14:20:38 +0100

    Update treeview example

--- examples/treeview.cpp
+++ examples/treeview.cpp
@@ -6,7 +6,7 @@ struct Hidden {
     Hidden(int id) : id{id} {}
 };
 
-int main(int argc, char **argv) { 
+int main(int argc, char **argv) {
     Application *app = Application::get();
         app->onReady = [&](Window *window) {
             if (argc > 1) {
@@ -30,12 +30,12 @@ int main(int argc, char **argv) {
                     tv->setMode(Mode::Unroll);
                 });
             h_box->append(unroll, Fill::Both);
-        app->append(h_box, Fill::Horizontal); 
+        app->append(h_box, Fill::Horizontal);
         tv = new TreeView<Hidden>();
             for (int i = 0; i < 3; i++) {
                 auto c = new Column<Hidden>(
                     "Column: " + std::to_string(i),
-                    nullptr, 
+                    nullptr,
                     HorizontalAlignment::Center,
                     [](TreeNode<Hidden> *lhs, TreeNode<Hidden> *rhs) {
                         return lhs->hidden->id > rhs->hidden->id;
@@ -48,13 +48,13 @@ int main(int argc, char **argv) {
             Tree<Hidden> *model = new Tree<Hidden>();
                 for (int i = 0; i < 1000; i++) {
                     std::vector<CellRenderer*> columns = {
-                        new TextCellRenderer("Row: " + std::to_string(i) + ", Column: 0"),
+                        new TextCellRenderer("Row: " + std::to_string(i) + "\nColumn: 0"),
                         new TextCellRenderer("Row: " + std::to_string(i) + ", Column: 1"),
-                        new TextCellRenderer("Row: " + std::to_string(i) + ", Column: 2"),
+                        new ImageTextCellRenderer((new Image(app->icons["close"]))->setForeground(COLOR_BLACK), "Row: " + std::to_string(i) + "\nColumn: 2"),
                     };
                     ((TextCellRenderer*)columns[0])->h_align = HorizontalAlignment::Left;
                     ((TextCellRenderer*)columns[1])->h_align = HorizontalAlignment::Right;
-                    ((TextCellRenderer*)columns[2])->h_align = HorizontalAlignment::Center;
+                    ((ImageTextCellRenderer*)columns[2])->h_align = HorizontalAlignment::Center;
                     TreeNode<Hidden> *node = new TreeNode<Hidden>(columns, new Hidden(i));
                     model->append(nullptr, node);
                 }
