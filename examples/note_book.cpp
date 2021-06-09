eacc6d834167d837330185f768f6519b4cb79142
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-06-09 08:52:04 +0100

    Less tabs more buttons, its just a little more aesthetic

--- examples/note_book.cpp
+++ examples/note_book.cpp
@@ -15,9 +15,9 @@ int main(int argc, char **argv) {
         app->setTitle("Notebook Test");
         NoteBook *nb = new NoteBook();
         {
-            for (int t = 1; t < 15; t++) {
+            for (int t = 1; t < 10; t++) {
                 Box *_b = new Box(Align::Vertical);
-                    for (int i = 0; i < 5; i++) {
+                    for (int i = 0; i < 10; i++) {
                         _b->append(new Button(std::to_string(i)), Fill::Both);
                     }
                 if (t % 3 == 0) {
