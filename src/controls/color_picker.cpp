43fdec49dfaedf153d6c25c778f86e00bdc33337
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-06-19 11:56:56 +0100

    Expand ColorPicker to include a LineEdit with the color and a label to preview it

--- src/controls/color_picker.cpp
+++ src/controls/color_picker.cpp
@@ -2,23 +2,27 @@
 #include "../application.hpp"
 
 ColorPicker::ColorPicker() {
+    m_color_edit = new LineEdit(COLOR_NONE.toString());
+    append(m_color_edit, Fill::Horizontal);
+    m_color_label = new Label("    ");
+    append(m_color_label, Fill::Vertical);
     onMouseDown.addEventListener([&](Widget *widget, MouseEvent event) {
         onMouseMotion.notify(widget, event);
     });
     onMouseMotion.addEventListener([&](Widget *widget, MouseEvent event) {
         if (isPressed()) {
             m_position = Point(event.x, event.y);
-            float texture_data[260*260*4] = {};
+            float texture_data[(int)(COLOR_PICKER_LENGTH * COLOR_PICKER_LENGTH * 4)] = {};
             glBindTexture(GL_TEXTURE_2D, Application::get()->icons["color_picker_gradient"]->ID);
             glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_FLOAT, texture_data);
             Point pos = Point(m_position.x - rect.x, m_position.y - rect.y);
-            if ((pos.x >= 0 && pos.x < 260) &&
-                (pos.y >= 0 && pos.y < 260)) {
+            if ((pos.x >= 0 && pos.x < COLOR_PICKER_LENGTH) &&
+                (pos.y >= 0 && pos.y < COLOR_PICKER_LENGTH)) {
                 m_color = Color(
-                    texture_data[(int)(pos.y * 260 * 4 + pos.x * 4 + 0)],
-                    texture_data[(int)(pos.y * 260 * 4 + pos.x * 4 + 1)],
-                    texture_data[(int)(pos.y * 260 * 4 + pos.x * 4 + 2)],
-                    texture_data[(int)(pos.y * 260 * 4 + pos.x * 4 + 3)]
+                    texture_data[(int)(pos.y * COLOR_PICKER_LENGTH * 4 + pos.x * 4 + 0)],
+                    texture_data[(int)(pos.y * COLOR_PICKER_LENGTH * 4 + pos.x * 4 + 1)],
+                    texture_data[(int)(pos.y * COLOR_PICKER_LENGTH * 4 + pos.x * 4 + 2)],
+                    texture_data[(int)(pos.y * COLOR_PICKER_LENGTH * 4 + pos.x * 4 + 3)]
                 );
             } else {
                 m_color = COLOR_NONE;
@@ -26,6 +30,10 @@ ColorPicker::ColorPicker() {
             if (onColorChanged) { onColorChanged(this, m_color); }
         }
     });
+    onColorChanged = [&](Widget *widget, Color color) {
+        m_color_edit->setText(color.toString());
+        m_color_label->style.widget_background = color;
+    };
 }
 
 ColorPicker::~ColorPicker() {}
@@ -40,25 +48,44 @@ void ColorPicker::draw(DrawingContext &dc, Rect rect, int state) {
     dc.setClip(rect.clipTo(old_clip));
     dc.drawTexture(
         Point(rect.x, rect.y),
-        sizeHint(dc),
+        Size(COLOR_PICKER_LENGTH, COLOR_PICKER_LENGTH),
         Application::get()->icons["color_picker_gradient"].get(),
         &m_coords,
         COLOR_WHITE
     );
 
     Color cur_color = COLOR_BLACK;
-    if (m_position.y >= rect.y + rect.h / 2) { cur_color = COLOR_WHITE; }
+    if (m_position.y >= rect.y + COLOR_PICKER_LENGTH / 2) { cur_color = COLOR_WHITE; }
     dc.fillRect(Rect(m_position.x - m_cursor_width / 2, m_position.y - m_cursor_width / 2, m_cursor_width, 1), cur_color);
     dc.fillRect(Rect(m_position.x - m_cursor_width / 2, m_position.y + m_cursor_width / 2, m_cursor_width, 1), cur_color);
     dc.fillRect(Rect(m_position.x - m_cursor_width / 2, m_position.y - m_cursor_width / 2, 1, m_cursor_width), cur_color);
     dc.fillRect(Rect(m_position.x + m_cursor_width / 2 - 1, m_position.y - m_cursor_width / 2, 1, m_cursor_width), cur_color);
+
+    // Step over color picker
+    rect.y += COLOR_PICKER_LENGTH;
+    rect.h -= COLOR_PICKER_LENGTH;
+    rect.w -= m_color_label->sizeHint(dc).w;
+    m_color_edit->draw(dc, rect, m_color_edit->state());
+
+    rect.x += rect.w;
+    rect.w = m_color_label->sizeHint(dc).w;
+    m_color_label->draw(dc, rect, m_color_label->state());
     dc.setClip(old_clip);
 }
 
 Size ColorPicker::sizeHint(DrawingContext &dc) {
-    return Size(260, 260);
+    Size s = Size(COLOR_PICKER_LENGTH, COLOR_PICKER_LENGTH);
+    float line = m_color_edit->sizeHint(dc).h;
+    float label = m_color_label->sizeHint(dc).h;
+    if (label > line) { s.h += label; }
+    else { s.h += line; }
+    return s;
 }
 
 Color ColorPicker::color() {
     return m_color;
+}
+
+bool ColorPicker::isLayout() {
+    return true;
 }
--- src/controls/color_picker.hpp
+++ src/controls/color_picker.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "widget.hpp"
+#include "label.hpp"
+#include "line_edit.hpp"
+
+#define COLOR_PICKER_LENGTH 260.0f
 
 class ColorPicker : public Widget {
     public:
@@ -11,10 +15,13 @@ class ColorPicker : public Widget {
         virtual const char* name() override;
         virtual void draw(DrawingContext &dc, Rect rect, int state) override;
         virtual Size sizeHint(DrawingContext &dc);
+        bool isLayout() override;
         Color color();
 
         Color m_color = COLOR_NONE;
         Point m_position;
         float m_cursor_width = 10;
         TextureCoordinates m_coords;
+        LineEdit *m_color_edit = nullptr;
+        Label *m_color_label = nullptr;
 };
