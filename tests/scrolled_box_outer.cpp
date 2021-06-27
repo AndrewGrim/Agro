cc68ab12cf64a9408a371ca7814720cbf20c8c26
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-06-27 15:01:55 +0100

    Add back color to test

--- tests/scrolled_box_outer.cpp
+++ tests/scrolled_box_outer.cpp
@@ -21,33 +21,41 @@ int main(int argc, char **argv) {
         app->resize(500, 500);
         Box *top = new Box(Align::Horizontal);
             ScrolledBox *av_fn = new ScrolledBox(Align::Vertical, Size(300, 300));
+                av_fn->style.window_background = COLOR_BLACK;
                 av_fn->append(new Button("P: Fill::None, C: Fill::None"), Fill::None);
                 av_fn->append(new Button("P: Fill::None, C: Fill::Horizontal"), Fill::Horizontal);
                 av_fn->append(new Button("P: Fill::None, C: Fill::Vertical"), Fill::Vertical);
                 av_fn->append(new Button("P: Fill::None, C: Fill::Both"), Fill::Both);
+                for (auto child : av_fn->children) { child->style.widget_background = Color("#ff5555"); }
             top->append(av_fn, Fill::None);
 
             ScrolledBox *av_fb = new ScrolledBox(Align::Vertical, Size(300, 300));
+                av_fb->style.window_background = COLOR_BLACK;
                 av_fb->append(new Button("P: Fill::Both, C: Fill::None"), Fill::None);
                 av_fb->append(new Button("P: Fill::Both, C: Fill::Horizontal"), Fill::Horizontal);
                 av_fb->append(new Button("P: Fill::Both, C: Fill::Vertical"), Fill::Vertical);
                 av_fb->append(new Button("P: Fill::Both, C: Fill::Both"), Fill::Both);
+                for (auto child : av_fb->children) { child->style.widget_background = Color("#ffaa55"); }
             top->append(av_fb, Fill::Both);
         app->append(top, Fill::Both);
 
         Box *bottom = new Box(Align::Horizontal);
             ScrolledBox *av_fh = new ScrolledBox(Align::Vertical, Size(300, 300));
+                av_fh->style.window_background = COLOR_BLACK;
                 av_fh->append(new Button("P: Fill::Horizontal, C: Fill::None"), Fill::None);
                 av_fh->append(new Button("P: Fill::Horizontal, C: Fill::Horizontal"), Fill::Horizontal);
                 av_fh->append(new Button("P: Fill::Horizontal, C: Fill::Vertical"), Fill::Vertical);
                 av_fh->append(new Button("P: Fill::Horizontal, C: Fill::Both"), Fill::Both);
+                for (auto child : av_fh->children) { child->style.widget_background = Color("#55ff55"); }
             bottom->append(av_fh, Fill::Horizontal);
 
             ScrolledBox *av_fv = new ScrolledBox(Align::Vertical, Size(300, 300));
+                av_fv->style.window_background = COLOR_BLACK;
                 av_fv->append(new Button("P: Fill::Vertical, C: Fill::None"), Fill::None);
                 av_fv->append(new Button("P: Fill::Vertical, C: Fill::Horizontal"), Fill::Horizontal);
                 av_fv->append(new Button("P: Fill::Vertical, C: Fill::Vertical"), Fill::Vertical);
                 av_fv->append(new Button("P: Fill::Vertical, C: Fill::Both"), Fill::Both);
+                for (auto child : av_fv->children) { child->style.widget_background = Color("#5555ff"); }
             bottom->append(av_fv, Fill::Vertical);
         app->append(bottom, Fill::Both);
     app->run();
