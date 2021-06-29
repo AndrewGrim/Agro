953e593aa8f054694149acaf1152b94bcbdea548
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-06-29 11:43:14 +0100

    Add basic CheckButton control

---
+++ src/controls/check_button.cpp
@@ -0,0 +1,70 @@
+#include "check_button.hpp"
+#include "../application.hpp"
+
+CheckButton::CheckButton(std::string text, bool is_checked) : Widget(), m_text{text}, m_is_checked{is_checked} {
+    m_checked_image = new Image(Application::get()->icons["check_button_checked"]);
+    m_unchecked_image = new Image(Application::get()->icons["check_button_unchecked"]);
+
+    onMouseClick.addEventListener([&](Widget *widget, MouseEvent event) {
+        if (m_is_checked) { m_is_checked = false; }
+        else { m_is_checked = true; }
+        onValueChanged.notify(m_is_checked);
+    });
+}
+
+CheckButton::~CheckButton() {
+    delete m_checked_image;
+    delete m_unchecked_image;
+}
+
+const char* CheckButton::name() {
+    return "CheckButton";
+}
+
+void CheckButton::draw(DrawingContext &dc, Rect rect, int state) {
+    dc.margin(rect, style);
+    this->rect = rect;
+    dc.padding(rect, style);
+
+    Image *image = m_is_checked ? m_checked_image : m_unchecked_image;
+    Color bg = m_is_checked ? dc.accentWidgetBackground(style) : dc.textBackground(style);
+    dc.fillRect(Rect(rect.x, rect.y + (rect.h / 2) - (m_size.h / 2), m_size.h, m_size.h), bg);
+    dc.drawTextureAligned(
+        Rect(rect.x, rect.y, m_size.h, rect.h),
+        Size(m_size.h, m_size.h),
+        image->_texture(),
+        image->coords(),
+        HorizontalAlignment::Left,
+        VerticalAlignment::Center,
+        dc.textSelected(style)
+    );
+    rect.x += m_size.h + m_padding;
+    rect.w -= m_size.h + m_padding;
+    dc.fillTextAligned(
+        font(),
+        m_text,
+        HorizontalAlignment::Left,
+        VerticalAlignment::Center,
+        rect,
+        0,
+        dc.textForeground(style),
+        m_tab_width
+    );
+}
+
+Size CheckButton::sizeHint(DrawingContext &dc) {
+    if (m_size_changed) {
+        Size s = dc.measureText(font(), m_text, m_tab_width);
+        s.w += m_padding;
+        if (s.h % 2 > 0) { s.h++; }
+        s.h += 4; // Make the icon slightly larger than text.
+        s.w += s.h; // The image size will be the square of the height.
+        m_size = s;
+
+        dc.sizeHintPadding(s, style);
+        dc.sizeHintMargin(s, style);
+
+        return s;
+    }
+    return m_size;
+}
