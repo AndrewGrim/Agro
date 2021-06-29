8aeb1cfa9405244165e609204ad63b3db22321c9
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-06-29 21:54:59 +0100

    Implement RadioButton widget

---
+++ src/controls/radio_button.cpp
@@ -0,0 +1,61 @@
+#include "radio_button.hpp"
+#include "../application.hpp"
+
+RadioButton::RadioButton(std::shared_ptr<RadioGroup> group, std::string text) : CheckButton(text, false) {
+    assert(group && "RadioGroup cannot be null!");
+    m_group = group;
+    group->buttons.push_back(this);
+    onMouseClick.listeners.clear();
+    delete m_checked_image;
+    delete m_unchecked_image;
+
+    m_checked_image = new Image(Application::get()->icons["radio_button_checked"]);
+    m_unchecked_image = new Image(Application::get()->icons["radio_button_unchecked"]);
+
+    onMouseClick.addEventListener([&](Widget *widget, MouseEvent event) {
+        for (auto child : m_group->buttons) {
+            child->m_is_checked = false;
+        }
+        m_is_checked = true;
+        m_group->onValueChanged.notify(this);
+    });
+}
+
+RadioButton::~RadioButton() {}
+
+const char* RadioButton::name() {
+    return "RadioButton";
+}
+
+void RadioButton::draw(DrawingContext &dc, Rect rect, int state) {
+    dc.margin(rect, style);
+    dc.padding(rect, style);
+    this->rect = rect;
+
+    Image *image = m_is_checked ? m_checked_image : m_unchecked_image;
+    Color check_image_bg = m_is_checked ? dc.accentWidgetBackground(style) : dc.borderBackground(style);
+    if (isHovered()) {
+        check_image_bg = m_is_checked ? check_image_bg : dc.accentHoveredBackground(style);
+    }
+    dc.drawTextureAligned(
+        Rect(rect.x, rect.y, m_size.h, rect.h),
+        Size(m_size.h, m_size.h),
+        image->_texture(),
+        image->coords(),
+        HorizontalAlignment::Left,
+        VerticalAlignment::Center,
+        check_image_bg
+    );
+    rect.x += m_size.h + m_padding;
+    rect.w -= m_size.h + m_padding;
+    dc.fillTextAligned(
+        font(),
+        m_text,
+        HorizontalAlignment::Left,
+        VerticalAlignment::Center,
+        rect,
+        0,
+        dc.textForeground(style),
+        m_tab_width
+    );
+}
---
+++ src/controls/radio_button.hpp
@@ -0,0 +1,25 @@
+#ifndef RADIO_BUTTON_HPP
+    #define RADIO_BUTTON_HPP
+
+    #include <string>
+
+    #include "check_button.hpp"
+
+    class RadioButton;
+
+    struct RadioGroup {
+        EventListener<RadioButton*> onValueChanged = EventListener<RadioButton*>();
+        std::vector<RadioButton*> buttons;
+        RadioGroup() {}
+    };
+
+    class RadioButton : public CheckButton {
+        public:
+            RadioButton(std::shared_ptr<RadioGroup> group, std::string text);
+            ~RadioButton();
+            virtual const char* name() override;
+            void draw(DrawingContext &dc, Rect rect, int state) override;
+
+            std::shared_ptr<RadioGroup> m_group;
+    };
+#endif
