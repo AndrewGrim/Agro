fddc1047ce03ddd12bcbd9ad12f98cce2005d4e9
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2021-10-08 23:11:32 +0100

    Change tooltip to have its own hardcoded look

--- src/controls/tooltip.cpp
+++ src/controls/tooltip.cpp
@@ -1,8 +1,13 @@
 #include "tooltip.hpp"
 
 Tooltip::Tooltip(std::string text) : Label(text) {
-    style.widget_background = Color("#ffffaa");
+    style.widget_background = Color("#ffff88");
+    style.text_foreground = COLOR_BLACK;
     style.border.type = STYLE_ALL;
+    style.border.color_top = COLOR_BLACK;
+    style.border.color_bottom = COLOR_BLACK;
+    style.border.color_left = COLOR_BLACK;
+    style.border.color_right = COLOR_BLACK;
 }
 
 Tooltip::~Tooltip() {
