f065920ab665938852bc8aa7e38d4d73f7e3ec0d
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2022-01-15 12:47:05 +0000

    Moved the styling code into its own widget

--- examples/widget_gallery.cpp
+++ examples/widget_gallery.cpp
@@ -14,11 +14,12 @@
 #include "../src/controls/check_button.hpp"
 #include "../src/controls/radio_button.hpp"
 #include "../src/controls/progress_bar.hpp"
+#include "../src/controls/style_editor.hpp"
 #include "resources.hpp"
 
 uint32_t timerCallback(uint32_t interval, void *data) {
     Application *app = (Application*)data;
-    ProgressBar *bar = (ProgressBar*)(app->mainWidget()->children[1]->children[0]->children[3]->children[0]);
+    ProgressBar *bar = (ProgressBar*)(app->mainWidget()->children[0]->children[0]->children[3]->children[0]);
     double value = bar->m_value;
     if (value < 1.0) {
         bar->m_value += 0.01;
@@ -244,125 +245,6 @@ Widget* treeView(Application &app) {
     return box;
 }
 
-Widget* styling(Application &app) {
-    ScrolledBox *box = new ScrolledBox(Align::Vertical);
-
-        Style &s = app.dc->default_style;
-
-        Button *reset = new Button("Reset");
-            reset->style.widget_background = Color("#ff5555");
-            reset->onMouseClick.addEventListener([&](Widget *button, MouseEvent event) {
-                app.dc->default_style = app.dc->default_light_style;
-                Application::get()->layout();
-            });
-        box->append(reset);
-
-        int *size_options[12] = {
-            &s.margin.top, &s.margin.bottom, &s.margin.left, &s.margin.right,
-            &s.border.top, &s.border.bottom, &s.border.left, &s.border.right,
-            &s.padding.top, &s.padding.bottom, &s.padding.left, &s.padding.right
-        };
-        std::string size_options_names[3] = {
-            "Margin", "Border", "Padding"
-        };
-        std::string size_options_sides_names[4] = {
-            "Top", "Bottom", "Left", "Right"
-        };
-        Box *size_box = new Box(Align::Horizontal);
-            for (int i = 0; i < 3; i++) {
-                GroupBox *gb = new GroupBox(Align::Vertical, size_options_names[i]);
-                    auto line = new LineEdit("", "All", 100);
-                    line->onTextChanged.addEventListener([=]() {
-                        if (line->text().length()) {
-                            try {
-                                int value = std::stoi(line->text().c_str());
-                                *size_options[i*4+0] = value;
-                                *size_options[i*4+1] = value;
-                                *size_options[i*4+2] = value;
-                                *size_options[i*4+3] = value;
-                                Application::get()->layout();
-                            } catch (std::invalid_argument &e) {
-                                warn("Parsing error!");
-                            } catch (std::out_of_range &e) {
-                                warn("Parsing error!");
-                            }
-                        } else {
-                            *size_options[i*4+0] = 0;
-                            *size_options[i*4+1] = 0;
-                            *size_options[i*4+2] = 0;
-                            *size_options[i*4+3] = 0;
-                        }
-                    });
-                    gb->append(line);
-                    for (int j = 0; j < 4; j++) {
-                        auto line = new LineEdit("", size_options_sides_names[j], 100);
-                        line->onTextChanged.addEventListener([=]() {
-                            if (line->text().length()) {
-                                try {
-                                    *size_options[i*4+j] = std::stoi(line->text().c_str());
-                                    Application::get()->layout();
-                                } catch (std::invalid_argument &e) {
-                                    warn("Parsing error!");
-                                } catch (std::out_of_range &e) {
-                                    warn("Parsing error!");
-                                }
-                            } else {
-                                *size_options[i*4+j] = 0;
-                            }
-                        });
-                        gb->append(line);
-                    }
-                size_box->append(gb);
-            }
-            Color *border_color[4] = {
-                &s.border.color_top, &s.border.color_bottom, &s.border.color_left, &s.border.color_right
-            };
-            GroupBox *gb = new GroupBox(Align::Vertical, "Border Color");
-                LineEdit *line = new LineEdit("", "All", 100);
-                    line->onTextChanged.addEventListener([=]() {
-                        Color value = Color(line->text().c_str());
-                        *border_color[0] = value;
-                        *border_color[1] = value;
-                        *border_color[2] = value;
-                        *border_color[3] = value;
-                    });
-            gb->append(line, Fill::Horizontal);
-                for (int i = 0; i < 4; i++) {
-                    LineEdit *line = new LineEdit("", size_options_sides_names[i], 100);
-                        line->onTextChanged.addEventListener([=]() {
-                            *border_color[i] = Color(line->text().c_str());
-                        });
-                    gb->append(line, Fill::Horizontal);
-                }
-            size_box->append(gb);
-        box->append(size_box, Fill::Horizontal);
-
-        Color *color_options[13] = {
-            &s.window_background, &s.widget_background, &s.accent_widget_background,
-            &s.text_foreground, &s.text_background, &s.text_selected, &s.text_disabled,
-            &s.hovered_background, &s.pressed_background, &s.accent_hovered_background,
-            &s.accent_pressed_background, &s.icon_foreground, &s.border_background
-        };
-        std::string color_option_names[13] = {
-            "window_background", "widget_background", "accent_widget_background",
-            "text_foreground", "text_background", "text_selected", "text_disabled",
-            "hovered_background", "pressed_background", "accent_hovered_background",
-            "accent_pressed_background", "icon_foreground", "border_background"
-        };
-        for (int i = 0; i < 13; i++) {
-            Label *l = new Label(color_option_names[i]);
-            box->append(l);
-
-            LineEdit *e = new LineEdit("", color_options[i]->toString(), 150);
-                e->onTextChanged.addEventListener([=]() {
-                    *color_options[i] = Color(e->text().c_str());
-                });
-            box->append(e, Fill::Horizontal);
-        }
-
-    return box;
-}
-
 int main(int argc, char **argv) {
     Application *app = Application::get();
         delete app->mainWidget();
@@ -377,27 +259,15 @@ int main(int argc, char **argv) {
         app->resize(800, 700);
         app->center();
         app->setTitle("Widget Gallery");
-        Box *h_box = new Box(Align::Horizontal);
-            auto light = new Button("Light");
-                light->onMouseClick.addEventListener([&](Widget *widget, MouseEvent event) {
-                    app->dc->default_style = app->dc->default_light_style;
-                });
-            h_box->append(light);
-            auto dark = new Button("Dark");
-                dark->onMouseClick.addEventListener([&](Widget *widget, MouseEvent event) {
-                    app->dc->default_style = app->dc->default_dark_style;
-                });
-            h_box->append(dark);
-        app->append(h_box);
         NoteBook *notebook = new NoteBook();
             notebook->appendTab(basic1(*app), "Basic 1", nullptr, false);
             notebook->appendTab(basic2(*app), "Basic 2", nullptr, false);
             notebook->appendTab(slidersAndScrollbars(*app), "Sliders and ScrollBars", nullptr, false);
             notebook->appendTab(splitter(*app), "Splitter", nullptr, false);
             notebook->appendTab(treeView(*app), "TreeView", nullptr, false);
-            notebook->appendTab(styling(*app), "Styling", nullptr, false);
         app->append(notebook, Fill::Both);
         SDL_AddTimer(100, timerCallback, app);
+        // StyleEditor::asWindow("Style Editor", Size(600, 600))->run();
     app->run();
 
     return 0;
