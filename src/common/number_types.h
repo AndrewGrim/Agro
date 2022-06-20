fb1d0643d3c5c514de57bafed87e3ea3eccd8b1c
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2022-06-20 11:01:38 +0100

    Add ptr sized integers to number_types

--- src/common/number_types.h
+++ src/common/number_types.h
@@ -7,11 +7,13 @@
     typedef uint16_t u16;
     typedef uint32_t u32;
     typedef uint64_t u64;
+    typedef uintptr_t usize;
 
     typedef int8_t i8;
     typedef int16_t i16;
     typedef int32_t i32;
     typedef int64_t i64;
+    typedef intptr_t isize;
 
     typedef float f32;
     typedef double f64;
