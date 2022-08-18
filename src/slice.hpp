436745d6fef56f0606151b7f3be20744ff02b640
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2022-08-18 19:13:05 +0100

    Add default ctor for Slice<T>

--- src/slice.hpp
+++ src/slice.hpp
@@ -7,6 +7,8 @@ template <typename T> struct Slice {
 	T *data = nullptr;
 	u64 length = 0;
 
+	Slice() {}
+
 	Slice(T *data, u64 length) : data{data}, length{length} {
 		assert(data && "Don't pass around invalid slices!");
 	}
