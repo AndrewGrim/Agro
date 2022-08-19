acf8967c854441ad62a67694ec5930ae61b96941
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2022-08-19 12:46:25 +0100

    Added JetBrainsMono font which will likely become the default for CodeEdit

--- examples/code_edit.cpp
+++ examples/code_edit.cpp
@@ -11,6 +11,7 @@ String loadFileToString(const char *path) {
     // }
     // fseek(file, 0, 0);
     u64 size = 8462111;
+    // u64 size = 64496;
     String s = String(size);
     fread(s.data(), sizeof(u8), size, file);
     s.data()[s.size()] = '\0';
@@ -28,8 +29,10 @@ int main(int argc, char **argv) {
         };
         app->mainWindow()->resize(600, 600);
         app->mainWindow()->center();
+        std::shared_ptr<Font> mono = std::shared_ptr<Font>(new Font(Application::get(), "fonts/JetBrainsMono/JetBrainsMono-Regular.ttf", 14, Font::Type::Mono));
         CodeEdit *edit = new CodeEdit(loadFileToString("../c_lexer/sqlite3.c"), Size(200, 200));
         // CodeEdit *edit = new CodeEdit(loadFileToString("src/controls/tree_view.hpp"), Size(200, 200));
+        edit->setFont(mono);
         app->mainWindow()->append(edit, Fill::Both);
     app->run();
 
