b09b50e06b361c64ab1e786c14017c64a8843a2f
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2022-08-27 10:48:16 +0100

    Update StopWatch to use QueryPerformanceCounter on Windows which is needed for microsecond resolution

--- src/stop_watch.hpp
+++ src/stop_watch.hpp
@@ -1,40 +1,76 @@
 #ifndef STOP_WATCH_HPP
     #define STOP_WATCH_HPP
 
-    #include <time.h>
-
     #include "common/number_types.h"
 
-    // TODO apparently on POSIX CLOCKS_PER_SEC is meant to be 1'000'000 but 1'000 on windows...
-    // we could try to use the performance counter on windows instead
-    struct StopWatch {
-        clock_t start_time = 0;
-        clock_t stop_time = 0;
-        clock_t duration = 0;
+    #ifdef __WIN32
+        #include <profileapi.h>
 
-        StopWatch() {}
+        struct StopWatch {
+            LARGE_INTEGER start_time;
+            LARGE_INTEGER stop_time;
+            LARGE_INTEGER duration;
+            LARGE_INTEGER frequency;
 
-        StopWatch start() {
-            start_time = clock();
-            return *this;
-        }
+            StopWatch() {
+                QueryPerformanceFrequency(&frequency);
+            }
 
-        StopWatch stop() {
-            stop_time = clock();
-            duration = stop_time - start_time;
-            return *this;
-        }
+            StopWatch start() {
+                QueryPerformanceCounter(&start_time);
+                return *this;
+            }
 
-        u64 asSeconds() {
-            return duration / CLOCKS_PER_SEC;
-        }
+            StopWatch stop() {
+                QueryPerformanceCounter(&stop_time);
+                duration.QuadPart = stop_time.QuadPart - start_time.QuadPart;
+                return *this;
+            }
 
-        u64 asMilliseconds() {
-            return duration / (CLOCKS_PER_SEC / 1'000);
-        }
+            u64 asSeconds() {
+                return duration.QuadPart / frequency.QuadPart;
+            }
 
-        u64 asMicroseconds() {
-            return duration / (CLOCKS_PER_SEC / 1'000'000);
-        }
-    };
+            u64 asMilliseconds() {
+                return duration.QuadPart * 1'000 / frequency.QuadPart;
+            }
+
+            u64 asMicroseconds() {
+                return duration.QuadPart * 1'000'000 / frequency.QuadPart;
+            }
+        };
+    #else
+        #include <time.h>
+
+        struct StopWatch {
+            clock_t start_time = 0;
+            clock_t stop_time = 0;
+            clock_t duration = 0;
+
+            StopWatch() {}
+
+            StopWatch start() {
+                start_time = clock();
+                return *this;
+            }
+
+            StopWatch stop() {
+                stop_time = clock();
+                duration = stop_time - start_time;
+                return *this;
+            }
+
+            u64 asSeconds() {
+                return duration / CLOCKS_PER_SEC;
+            }
+
+            u64 asMilliseconds() {
+                return duration / (CLOCKS_PER_SEC / 1'000);
+            }
+
+            u64 asMicroseconds() {
+                return duration / (CLOCKS_PER_SEC / 1'000'000);
+            }
+        };
+    #endif
 #endif
