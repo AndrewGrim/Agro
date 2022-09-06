a10e2defc1b99bbf4997d825a67c1ba8ddbe1f87
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2022-09-06 15:52:40 +0100

    Update to showcase recursively traversing a dir and listing contents

--- tests/fs.cpp
+++ tests/fs.cpp
@@ -1,13 +1,24 @@
 #include "../src/application.hpp"
 #include "../src/core/fs.hpp"
 
-int main(int argc, char **argv) {
-    Dir dir(".");
+// TODO would be nice to be able to apply a custom filter
+void recurseDir(String path, u32 depth = 0) {
+    Dir dir(path);
     auto iter = dir.iter();
     while ((iter = iter.next())) {
-        printf("entry name: %s, ", iter.entry.name.data());
-        printf("entry type: %d\n", (int)iter.entry.type);
+        for (u32 i = 0; i < depth; i++) {
+            printf(" ");
+        }
+        printf("%d ", (int)iter.entry.type);
+        printf("%s\n", iter.entry.name.data());
+        if (iter.entry.type == Dir::Iterator::Entry::Type::Directory) {
+            String child_path = String::format("%s/%s", dir.path.data(), iter.entry.name.data());
+            recurseDir(child_path, depth + 4);
+        }
     }
+}
 
+int main(int argc, char **argv) {
+    recurseDir(".");
     return 0;
 }
