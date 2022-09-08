acbaed01c084745b1a9545d1a176ca45a5991f15
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2022-09-08 11:31:18 +0100

    Change to utf8::validate and File::load

--- examples/code_edit.cpp
+++ examples/code_edit.cpp
@@ -6,30 +6,10 @@
 #include "../src/controls/tree_view.hpp"
 #include "../src/controls/splitter.hpp"
 
-bool basicValidate(const String &text) {
-    for (u64 i = 0; i < text.size();) {
-        u8 len = utf8::length(text.data() + i);
-        if (!len) { return false; }
-        i += len;
-    }
-    return true;
-}
-
-String loadFileToString(const char *path) {
-    FILE *file = fopen(path, "rb");
-    u64 size = File::getSize(path).unwrap();
-    String s = String(size);
-    fread(s.data(), sizeof(u8), size, file);
-    s.data()[s.size()] = '\0';
-    if (!basicValidate(s)) { return String("<NOT VALID UTF-8>"); }
-    return s;
-}
-
 struct Hidden {
     Dir::Iterator::Entry::Type type;
     Hidden(Dir::Iterator::Entry::Type type) : type{type} {}
 };
-
 
 // TODO would be nice if they were sorted alphabetically
 void recurseDir(Tree<Hidden> *model, TreeNode<Hidden> *parent_node, String path) {
@@ -96,15 +76,14 @@ int main(int argc, char **argv) {
                         path.insert(0, ((ImageTextCellRenderer*)current->columns[0])->text);
                     }
                     if (File::getSize(path)) {
-                        edit->setText(loadFileToString(path.data()));
+                        edit->setText(File::load(path).unwrap());
                     }
                 });
             split->left(tv);
         }
         {
             std::shared_ptr<Font> mono = std::shared_ptr<Font>(new Font(Application::get(), "fonts/JetBrainsMono/JetBrainsMono-Regular.ttf", 14, Font::Type::Mono));
-            // edit = new CodeEdit(loadFileToString("src/controls/code_edit.cpp"), Size(200, 200));
-            edit = new CodeEdit("", Size(200, 200));
+            edit = new CodeEdit(File::load("src/core/string.cpp").unwrap(), Size(200, 200));
             edit->setFont(mono);
             split->right(edit);
         }
